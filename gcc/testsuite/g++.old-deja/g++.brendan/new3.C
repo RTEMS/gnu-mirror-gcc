27b9e4aa5da7bca314a80cbe734b26089f3b4c7e
No Author <no-author@gcc.gnu.org>
1997-12-01 22:33:46 +0000

    This commit was manufactured by cvs2svn to create tag 'egcs_ss_971201'.
    From-SVN: r16877

--- gcc/testsuite/g++.old-deja/g++.brendan/new3.C
+++ gcc/testsuite/g++.old-deja/g++.brendan/new3.C
@@ -1,35 +1,3 @@
-// GROUPS passed operator-new
-#include <stdio.h>
-#include <stdlib.h>
-
-int pass = 0;
-
+#include <new>
+void *operator new(size_t sz) throw (std::bad_alloc) {
 void *operator new(size_t sz){
-
-  void *p;
-
-  pass = 1;
-  p = malloc(sz);
-  return p;
-}
-
-class A {
-public:
-  A() {}
-  ~A() {}
-
-  int a;
-  int b;
-};
-
-
-int main()
-{
-  A *bb = new A[10];
-  delete [] bb;
-
-  if (pass)
-    printf ("PASS\n");
-  else
-    printf ("FAIL\n");
-}
---
+++ gcc/version.c
@@ -0,0 +1,1 @@
+char *version_string = "egcs-2.90.20 971201 (gcc2-970802 experimental)";
