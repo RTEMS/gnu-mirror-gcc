e7896990320753de822504dfab6f02a1807cd310
No Author <no-author@gcc.gnu.org>
1997-12-06 15:48:29 +0000

    This commit was manufactured by cvs2svn to create tag 'before-merge-gcc-2-8'.
    From-SVN: r16979

---
+++ gcc/acconfig.h
@@ -0,0 +1,15 @@
+/* Define if you have a working <inttypes.h> header file.  */
+#undef HAVE_INTTYPES_H
+
+/* Whether malloc must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_MALLOC
+
+/* Whether realloc must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_REALLOC
+
+/* Whether calloc must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_CALLOC
+
+/* Whether free must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_FREE
+@TOP@
---
+++ gcc/version.c
@@ -0,0 +1,1 @@
+char *version_string = "egcs-2.91.00 971101 (gcc2-971021 experimental)";
