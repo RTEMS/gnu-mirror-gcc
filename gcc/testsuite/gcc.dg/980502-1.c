9659eeed95d3c655d497aadf46a63bb229e19f9a
No Author <no-author@gcc.gnu.org>
1998-05-15 23:48:56 +0000

    This commit was manufactured by cvs2svn to create tag 'before-merge-etc-980515'.
    From-SVN: r19793

--- gcc/testsuite/gcc.dg/980502-1.c
+++
@@ -1,9 +0,0 @@
-/* { dg-do compile  }*/
-/* { dg-options "-O2" } */
-
-void f(void)
-{
-        char *const line = "/dev/ptyXX";
-        line[8] = 1;
-        return line;
-}
--- gcc/version.c
+++
@@ -1,1 +0,0 @@
-char *version_string = "egcs-2.91.28 19980508 (gcc2 ss-980502 experimental)";
