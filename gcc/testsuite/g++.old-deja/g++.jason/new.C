698f7a751d4530f02965e63beec3a73186a02b16
No Author <no-author@gcc.gnu.org>
1998-12-04 11:26:43 +0000

    This commit was manufactured by cvs2svn to create tag 'before1204merge'.
    From-SVN: r24088

--- gcc/testsuite/g++.old-deja/g++.jason/new.C
+++ gcc/testsuite/g++.old-deja/g++.jason/new.C
@@ -1,11 +1,11 @@
 // Bug: new doesn't make sure that the count is an integral value.
 
-typedef __SIZE_TYPE__ size_t;
+#include <new>
 extern "C" int printf (const char *, ...);
 extern "C" void *malloc (size_t);
 size_t s;
 
-void * operator new (size_t siz) {
+void * operator new (size_t siz) throw (std::bad_alloc) {
   if (s == 0)
     s = siz;
   else
@@ -19,7 +19,7 @@ main()
 
   float f = 3;
   int* b1 = new int[(int)f];
-  int* b2 = new int[f];
+  int* b2 = new int[f];		// ERROR - new requires integral size
 
   return s;
 }
--- gcc/testsuite/g++.old-deja/g++.jason/template18.C
+++ gcc/testsuite/g++.old-deja/g++.jason/template18.C
@@ -1,16 +1,18 @@
-// Build don't link: 
-// Special g++ Options: -g -fexternal-templates
 // GROUPS passed templates
 // Bug: g++ emits template instances when it shouldn't.
 // Special g++ Options: -g -fexternal-templates
 
+// We mark this XFAIL because we can't test for expected linker errors.
+// If we get an XPASS for this testcase, that's a bug.
+// (OK) excess errors test - XFAIL *-*-*
+
 #pragma implementation "irrelevant_file"
-#line 1 "wa.h"
-#pragma interface		// ERROR - , XFAIL *-*-*
+#line 1 "template18.h"
+#pragma interface
 template <class T> inline T min (T a, T b) { return a<b?a:b; }
-#line 3 "wa.C" 
+#line 13 "template18.C"
 
 main()
 {
-  min (1, 1); 
-}// UNKNOWN "min"
+  min (1, 1); 		// should produce an undefined symbol error.
+}
