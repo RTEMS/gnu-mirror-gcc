6b95e991023f760a4ddf0b8a1d9ca2cc7fbc31de
No Author <no-author@gcc.gnu.org>
2003-09-22 07:11:27 +0000

    This commit was manufactured by cvs2svn to create tag 'gcc-ss-3_3-20030922'.
    From-SVN: r71647

--- gcc/config/alpha/linux.h
+++ gcc/config/alpha/linux.h
@@ -59,6 +59,11 @@ Boston, MA 02111-1307, USA.  */
 /* Define this so that all GNU/Linux targets handle the same pragmas.  */
 #define HANDLE_PRAGMA_PACK_PUSH_POP
 
+#define TARGET_HAS_F_SETLKW
+
+#define LINK_GCC_C_SEQUENCE_SPEC \
+  "%{static:--start-group} %G %L %{static:--end-group}%{!static:%G}"
+
 /* Do code reading to identify a signal frame, and set the frame
    state data appropriately.  See unwind-dw2.c for the structs.  */
 
