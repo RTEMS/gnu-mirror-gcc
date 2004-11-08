59fba6ae00652e487e400f682b7bb75375c0ed8a
No Author <no-author@gcc.gnu.org>
2004-11-08 07:29:20 +0000

    This commit was manufactured by cvs2svn to create tag 'before-merge-3_4-csl-sol210-3_4-1'.
    From-SVN: r90259

---
+++ gcc/testsuite/gcc.dg/i386-sse-8.c
@@ -0,0 +1,14 @@
+/* PR target/14313 */
+/* Origin: <Pawe Sikora <pluto@ds14.agh.edu.pl> */
+
+/* { dg-do compile } */
+/* { dg-forbid-option "-m64" } */
+/* { dg-options "-march=pentium3" { target i?86-*-* x86_64-*-* } } */
+
+int main() 
+{ 
+  typedef int v __attribute__ ((mode(V2DI))); 
+  v a, b; 
+  a = b; 
+  return 0; 
+}
