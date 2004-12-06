d1e75e2f4f636b02759572b89ab7dcf2b110347a
No Author <no-author@gcc.gnu.org>
2004-12-06 00:16:20 +0000

    This commit was manufactured by cvs2svn to create tag 'libstdcxx_so_7-merge-20041206'.
    From-SVN: r91762

--- gcc/testsuite/gcc.dg/pr18596-1.c
+++
@@ -1,7 +0,0 @@
-/* { dg-do compile } */
-int f(int i)
-{
-  static int g(); /* { dg-warning "invalid storage class" } */
-  static int g() { return i; } /* { dg-warning "invalid storage class" } */
-  return g();
-}
