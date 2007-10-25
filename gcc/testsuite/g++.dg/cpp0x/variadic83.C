54326e4bb1ccbb32a1cd5272d6384fc94f2dd5b1
Johannes Singler <singler@gcc.gnu.org>
2007-10-25 15:54:27 +0000

    re PR libstdc++/33893 ([parallel mode] Algorithms rely on omp_set_dynamic(false))
    2007-10-25  Johannes Singler
    Branched libstdc++-v3 to fix OpenMP issues separately (PR 33893)
    
    From-SVN: r129626

--- gcc/testsuite/g++.dg/cpp0x/variadic83.C
+++
@@ -1,8 +0,0 @@
-// PR c++/31441
-// { dg-options "-std=gnu++0x" }
-
-template<typename> struct A;
-
-template<typename... T> struct A<T...> { }; // { dg-error "cannot expand" }
-
-A<int> a; // { dg-error "incomplete type" }
