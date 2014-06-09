c682bb83384c79722635112fbb6dd52f372a41be
Michael Meissner <meissner@gcc.gnu.org>
2014-06-09 16:40:24 +0000

    undelete branch
    [[Split portion of a mixed commit.]]
    
    From-SVN: r211379.11

---
+++ libgcc/config/rs6000/float128-vsx.h
@@ -0,0 +1,30 @@
+/* Map __<func>tf<n> to the name used for __float128.  */
+
+#define __addtf3	__addkf3
+#define __subtf3	__subkf3
+#define __multf3	__mulkf3
+#define __divtf3	__divkf3
+#define __eqtf2		__eqkf2
+#define __netf2		__nekf2
+#define __getf2		__gekf2
+#define __gttf2		__gtkf2
+#define __letf2		__lekf2
+#define __lttf2		__ltkf2
+#define __unordtf2	__unordkf2
+#define __negtf2	__negkf2
+#define __extenddftf2	__extenddfkf2
+#define __extendsftf2	__extendsfkf2
+#define __trunctfdf2	__trunckfdf2
+#define __trunctfsf2	__trunckfsf2
+#define __fixtfsi	__fixkfsi
+#define __fixtfdi	__fixkfdi
+#define __fixtfti	__fixkfti
+#define __fixunstfsi	__fixunskfsi
+#define __fixunstfdi	__fixunskfdi
+#define __fixunstfti	__fixunskfti
+#define __floatsitf	__floatsikf
+#define __floatditf	__floatdikf
+#define __floattitf	__floattikf
+#define __floatunsitf	__floatunsikf
+#define __floatunditf	__floatundikf
+#define __floatuntitf	__floatuntikf
