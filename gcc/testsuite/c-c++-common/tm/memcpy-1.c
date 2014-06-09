d6c05ed1483599eecae8c5ec268874b2ba6b8726
Michael Meissner <meissner@gcc.gnu.org>
2014-06-09 16:31:26 +0000

    delete branch
    [[Split portion of a mixed commit.]]
    
    From-SVN: r211378.20

--- gcc/testsuite/c-c++-common/tm/memcpy-1.c
+++
@@ -1,9 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-fgnu-tm" } */
-#include <string.h>
-
-__attribute__((transaction_safe))
-void *wmemcpy(void *dest, const void *src, size_t n)
-{
-    return memcpy(dest, src, n);
-}
