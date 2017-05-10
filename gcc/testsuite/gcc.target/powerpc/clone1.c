5a2e8554176c1506c57d102a221b568063a2d1cd
Michael Meissner <meissner@gcc.gnu.org>
2017-05-10 22:12:31 +0000

    fix clone1.c test #2
    From-SVN: r247867

--- gcc/testsuite/gcc.target/powerpc/clone1.c
+++ gcc/testsuite/gcc.target/powerpc/clone1.c
@@ -14,6 +14,6 @@ long mod_func_or (long a, long b, long c)
   return mod_func (a, b) | c;
 }
 
-/* { dg-final { scan-assembler-times {\mdivd\M}  1 } } */
-/* { dg-final { scan-assembler-times {\mmulld\M} 1 } } */
+/* { dg-final { scan-assembler-times {\mdivd\M}  3 } } */
+/* { dg-final { scan-assembler-times {\mmulld\M} 3 } } */
 /* { dg-final { scan-assembler-times {\mmodsd\M} 1 } } */
