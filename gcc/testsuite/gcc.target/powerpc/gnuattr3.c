52f669703d3be9ea13aadc5652a106b8508ae4ec
Michael Meissner <meissner@gcc.gnu.org>
2018-01-11 01:07:39 +0000

    fix test
    From-SVN: r256473

--- gcc/testsuite/gcc.target/powerpc/gnuattr3.c
+++ gcc/testsuite/gcc.target/powerpc/gnuattr3.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target { powerpc*-linux-* && lp64 } } } */
 /* { dg-require-effective-target powerpc_vsx_ok } */
 /* { dg-options "-O2 -mvsx -mabi=ibmlongdouble -Wno-psabi" } */
-/* { dg-final { scan-assembler "gnu_attribute 4, 9" } } */
+/* { dg-final { scan-assembler "gnu_attribute 4, 5" } } */
 
 /* Check that if we can do the long double operation without doing an emulator
    call, such as with copysign, that we still set the appropriate
