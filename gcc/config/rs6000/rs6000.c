76ed411466235a2ef2fd1f1ceaae338745e4400e
Michael Meissner <meissner@linux.ibm.com>
2019-07-25 02:52:20 +0000

    Load up addresses using pc-relative support.
    2019-07-24  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/rs6000.c (rs6000_emit_move): Add support to load
    	up addresses if we have pc-relative support.
    	* config/rs6000/rs6000.md (pcrel_addr): New insn.
    	(pcrel_ext_addr): New insn.
    
    From-SVN: r273786

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -9779,6 +9779,20 @@ rs6000_emit_move (rtx dest, rtx source, machine_mode mode)
 	  return;
 	}
 
+      /* Handle pc-relative addresses, either external symbols or internal
+	 within the function.  */
+      if (TARGET_PCREL)
+	{
+	  const unsigned addr_flags = (ADDR_VALIDATE_PCREL_LOCAL
+				       | ADDR_VALIDATE_PCREL_EXT);
+
+	  if (addr_validate_p (operands[1], INSN_FORM_PREFIXED, addr_flags))
+	    {
+	      emit_insn (gen_rtx_SET (operands[0], operands[1]));
+	      return;
+	    }
+	}
+
       if (DEFAULT_ABI == ABI_V4
 	  && mode == Pmode && mode == SImode
 	  && flag_pic == 1 && got_operand (operands[1], mode))
