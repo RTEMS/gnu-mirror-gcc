440d419e2b383214ebfc6b0df150e30b613fc5dd
Michael Meissner <meissner@linux.ibm.com>
2019-09-18 22:06:38 +0000

    Add tests.
    2019-09-18  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc/testsuite/gcc.target/powerpc/prefix-large.h: New set of
    	tests to test prefixed addressing on 'future' system with
    	pc-relative addreses.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-dd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-df.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-di.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-hi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-kf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-qi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-sd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-sf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-si.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-udi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-uhi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-uqi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-usi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-pcrel-v2df.c: New test.
    
    2019-09-18  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc/testsuite/gcc.target/powerpc/prefix-large.h: New set of
    	tests to test prefixed addressing on 'future' system with large
    	numeric offsets.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-dd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-df.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-di.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-hi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-kf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-qi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-sd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-sf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-si.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-udi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-uhi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-uqi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-usi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-v2df.c: New test.
    
    2019-09-18  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc/testsuite/gcc.target/powerpc/prefix-odd-memory.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/paddi-1.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/paddi-2.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/paddi-3.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-premodify.c: New test.
    
    From-SVN: r275917

---
+++ gcc/testsuite/gcc.target/powerpc/prefix-pcrel-sd.c
@@ -0,0 +1,12 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-require-effective-target powerpc_future_ok } */
+/* { dg-options "-O2 -mdejagnu-cpu=future" } */
+
+/* Tests for prefixed instructions testing whether pc-relative prefixed
+   instructions are generated for SImode.  */
+
+#define TYPE _Decimal32
+
+#include "prefix-pcrel.h"
+
+/* { dg-final { scan-assembler-times {\mpaddi|\mpla\M} 3 } } */
