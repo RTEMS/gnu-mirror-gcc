cd1508314b5729caedc71bd9621a2c01f62df568
Michael Meissner <meissner@linux.ibm.com>
2019-09-30 17:18:14 +0000

    Add prefixed memory tests with large offsets.
    2019-09-30  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc/testsuite/gcc.target/powerpc/prefix-large.h: New set of
    	tests to test prefixed addressing on 'future' system with large
    	numeric offsets.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-dd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-df.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-di.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-hi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-kf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-qi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-sd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-sf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-si.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-udi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-uhi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-uqi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-usi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-v2df.c: New test.
    
    From-SVN: r276356

---
+++ gcc/testsuite/gcc.target/powerpc/prefix-large-uqi.c
@@ -0,0 +1,13 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-require-effective-target powerpc_future_ok } */
+/* { dg-options "-O2 -mdejagnu-cpu=future" } */
+
+/* Tests for prefixed instructions testing whether we can generate a prefixed
+   load/store instruction that has a 34-bit offset.  */
+
+#define TYPE unsigned char
+
+#include "prefix-large.h"
+
+/* { dg-final { scan-assembler-times {\mplbz\M}  2 } } */
+/* { dg-final { scan-assembler-times {\mpstb\M}  2 } } */
---
+++ gcc/testsuite/gcc.target/powerpc/prefix-large-v2df.c
@@ -0,0 +1,13 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-require-effective-target powerpc_future_ok } */
+/* { dg-options "-O2 -mdejagnu-cpu=future" } */
+
+/* Tests for prefixed instructions testing whether we can generate a prefixed
+   load/store instruction that has a 34-bit offset.  */
+
+#define TYPE vector double
+
+#include "prefix-large.h"
+
+/* { dg-final { scan-assembler-times {\mplxv\M}  2 } } */
+/* { dg-final { scan-assembler-times {\mpstxv\M} 2 } } */
