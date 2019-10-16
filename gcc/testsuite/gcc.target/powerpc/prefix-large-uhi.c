9996471b816ef8f58f16995a5e769cd1b3048e7d
Michael Meissner <meissner@linux.ibm.com>
2019-10-16 02:54:24 +0000

    Add prefix memory tests with large offsets.
    2019-10-15  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc/testsuite/gcc.target/powerpc/prefix-large.h: New set of
    	tests to test prefixed addressing on 'future' system with large
    	numeric offsets.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-dd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-df.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-di.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-hi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-kf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-qi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-sd.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-sf.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-si.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-udi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-uhi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-uqi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-usi.c: New test.
    	* gcc/testsuite/gcc.target/powerpc/prefix-large-v2df.c: New test.
    
    From-SVN: r277042

---
+++ gcc/testsuite/gcc.target/powerpc/prefix-large-uhi.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target powerpc_prefixed_addr_ok } */
+/* { dg-options "-O2 -mdejagnu-cpu=future" } */
+
+/* Tests for prefixed instructions testing whether we can generate a prefixed
+   load/store instruction that has a 34-bit offset.  */
+
+#define TYPE unsigned short
+
+#include "prefix-large.h"
+
+/* { dg-final { scan-assembler-times {\mplhz\M}  2 } } */
+/* { dg-final { scan-assembler-times {\mpsth\M}  2 } } */
