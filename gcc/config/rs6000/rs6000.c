24e1eb335b9b318f2b3abd6ad77ea2ef40cbbf86
Michael Meissner <meissner@gcc.gnu.org>
2019-12-10 21:05:40 +0000

    Optimize vector extracts with large offsets for -mcpu=future.
    2019-12-10  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/rs6000.c (rs6000_adjust_vec_address): Add support
    	for the offset being 34-bits when -mcpu=future is used.
    
    From-SVN: r279195

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -6766,9 +6766,17 @@ rs6000_adjust_vec_address (rtx scalar_reg,
 	  HOST_WIDE_INT offset = INTVAL (op1) + INTVAL (element_offset);
 	  rtx offset_rtx = GEN_INT (offset);
 
-	  if (IN_RANGE (offset, -32768, 32767)
+	  /* 16-bit offset.  */
+	  if (SIGNED_16BIT_OFFSET_P (offset)
 	      && (scalar_size < 8 || (offset & 0x3) == 0))
 	    new_addr = gen_rtx_PLUS (Pmode, op0, offset_rtx);
+
+	  /* 34-bit offset if we have prefixed addresses.  */
+	  else if (TARGET_PREFIXED_ADDR && SIGNED_34BIT_OFFSET_P (offset))
+	    new_addr = gen_rtx_PLUS (Pmode, op0, offset_rtx);
+
+	  /* Offset overflowed, move offset to the temporary (which will likely
+	     be split), and do X-FORM addressing.  */
 	  else
 	    {
 	      emit_move_insn (base_tmp, offset_rtx);
