44c806b82d5d2972887a8cd3079db27c408748f8
Michael Meissner <meissner@linux.ibm.com>
2019-12-19 23:57:23 +0000

    Test vec_extract of a PC-relative address with -mcpu=future
    2019-12-19  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/vec-extract-pcrel-si.c: New test for
    	vec_extract from a PC-relative address.
    	* gcc.target/powerpc/vec-extract-pcrel-di.c: New test for
    	vec_extract from a PC-relative address.
    	* gcc.target/powerpc/vec-extract-pcrel-sf.c: New test for
    	vec_extract from a PC-relative address.
    	* gcc.target/powerpc/vec-extract-pcrel-df.c: New test for
    	vec_extract from a PC-relative address.
    
    From-SVN: r279616

---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-pcrel-di.c
@@ -0,0 +1,37 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target powerpc_pcrel } */
+/* { dg-options "-O2 -mdejagnu-cpu=future" } */
+
+/* Test if we can support vec_extract on V2DI vectors with a PC-relative
+   address.  */
+
+#include <altivec.h>
+
+#ifndef TYPE
+#define TYPE unsigned long
+#endif
+
+static vector TYPE v;
+vector TYPE *p = &v;
+
+TYPE
+get0 (void)
+{
+  return vec_extract (v, 0);
+}
+
+TYPE
+get1 (void)
+{
+  return vec_extract (v, 1);
+}
+
+TYPE
+getn (unsigned long n)
+{
+  return vec_extract (v, n);
+}
+
+/* { dg-final { scan-assembler-times {[@]pcrel}  3 } } */
+/* { dg-final { scan-assembler-times {\mpld\M}   2 } } */
+/* { dg-final { scan-assembler-times {\mpla\M}   1 } } */
