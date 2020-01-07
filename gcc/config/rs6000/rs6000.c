1342027e169f6d70d8aa37d3f721aadb28e4b9d3
Michael Meissner <meissner@linux.ibm.com>
2020-01-07 21:03:34 +0000

    Add gcc_asserts for rs6000_adjust_vec_address.
    From-SVN: r279968

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -6771,6 +6771,9 @@ rs6000_adjust_vec_address (rtx scalar_reg,
   rtx new_addr;
   bool valid_addr_p;
 
+  gcc_assert (!reg_mentioned_p (base_tmp, addr));
+  gcc_assert (!reg_mentioned_p (base_tmp, element));
+
   /* Vector addresses should not have PRE_INC, PRE_DEC, or PRE_MODIFY.  */
   gcc_assert (GET_RTX_CLASS (GET_CODE (addr)) != RTX_AUTOINC);
 
@@ -6780,6 +6783,10 @@ rs6000_adjust_vec_address (rtx scalar_reg,
     element_offset = GEN_INT (INTVAL (element) * scalar_size);
   else
     {
+      /* If the element number is variable, make sure the address is just a
+	 single base register.  All insns should use the 'Q' constraint.  */
+      gcc_assert (REG_P (element) || SUBREG_P (element));
+
       int byte_shift = exact_log2 (scalar_size);
       gcc_assert (byte_shift >= 0);
 
