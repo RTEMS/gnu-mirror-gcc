a52204e0687b5e597796100f8fe8d8621afe8262
Michael Meissner <meissner@linux.ibm.com>
2020-01-09 23:42:53 +0000

    Add vec_extract tests for -mcpu=future with large offsets.
    2020-01-09  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/vec-extract-large-si.c: New test for
    	vec_extract from a vector unsigned int in memory with a large
    	offset.
    	* gcc.target/powerpc/vec-extract-large-di.c: New test for
    	vec_extract from a vector long in memory with a large offset.
    	* gcc.target/powerpc/vec-extract-large-sf.c: New test for
    	vec_extract from a vector float in memory with a large offset.
    	* gcc.target/powerpc/vec-extract-large-df.c: New test for
    	vec_extract from a vector double in memory with a large offset.
    
    From-SVN: r280093

---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-large-df.c
@@ -0,0 +1,37 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target powerpc_prefixed_addr } */
+/* { dg-options "-O2 -mdejagnu-cpu=future" } */
+
+/* Test if we can support vec_extract on V2DF vectors with a large numeric
+   offset address.  */
+
+#include <altivec.h>
+
+#ifndef TYPE
+#define TYPE double
+#endif
+
+#ifndef OFFSET
+#define OFFSET 0x12345
+#endif
+
+TYPE
+get0 (vector TYPE *p)
+{
+  return vec_extract (p[OFFSET], 0);
+}
+
+TYPE
+get1 (vector TYPE *p)
+{
+  return vec_extract (p[OFFSET], 1);
+}
+
+TYPE
+getn (vector TYPE *p, unsigned long n)
+{
+  return vec_extract (p[OFFSET], n);
+}
+
+/* { dg-final { scan-assembler-times {\mplfd\M}  2 } } */
+/* { dg-final { scan-assembler-times {\mpaddi\M} 1 } } */
