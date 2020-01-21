9bc2b7c3cbb9b80d8837d69bf19206b20a3414dc
Patrick Palka <ppalka@gcc.gnu.org>
2020-01-21 16:53:01 -0500

    Use ranges::iter_swap instead of std::swap in ranges::swap_ranges

--- libstdc++-v3/include/bits/ranges_algo.h
+++ libstdc++-v3/include/bits/ranges_algo.h
@@ -953,7 +953,7 @@ namespace ranges
       {
 	for (; __first1 != __last1 && __first2 != __last2;
 	     ++__first1, (void)++__first2)
-	  std::swap(*__first1, *__first2);
+	  ranges::iter_swap(__first1, __first2);
 	return {__first1, __first2};
       }
 
