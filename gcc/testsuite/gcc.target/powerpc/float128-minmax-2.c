00b576c99be6c5de394bf7b53a494accbccbbad3
Michael Meissner <meissner@linux.ibm.com>
2020-06-23 12:05:53 -0400

    PowerPC: Add power10 IEEE 128-bit min/max/cmove.
    gcc/
    2020-06-23  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/rs6000.c (rs6000_emit_hardware_fp_minmax): Update
    	comment.
    	(rs6000_emit_hardware_fp_cmove): Update comment.
    	(rs6000_emit_cmove): Add support for IEEE 128-bit min, max, and
    	comparisons with -mcpu=future.
    	(rs6000_emit_minmax): Add support for IEEE 128-bit min/max with
    	-mcpu=future.
    	* config/rs6000/rs6000.md (s<minmax><mode>3, IEEE128 iterator):
    	New insns for IEEE 128-bit min/max.
    	(mov<mode>cc, IEEE128 iterator): New insns for IEEE 128-bit
    	conditional move.
    	(mov<mode>cc_future, IEEE128 iterator): New insns for IEEE 128-bit
    	conditional move.
    	(mov<mode>cc_invert_future, IEEE128 iterator): New insns for IEEE
    	128-bit conditional move.
    	(fpmask<mode>, IEEE128 iterator): New insns for IEEE 128-bit
    	conditional move.
    
    gcc/testsuite/
    2020-06-23  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/float128-minmax-2.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/float128-minmax-2.c
@@ -0,0 +1,70 @@
+/* { dg-do compile { target { powerpc*-*-* && lp64 } } } */
+/* { dg-require-effective-target powerpc_power10_ok } */
+/* { dg-options "-mdejagnu-cpu=power10 -O2 -ffast-math" } */
+/* { dg-final { scan-assembler-not "xscmpuqp"  } } */
+/* { dg-final { scan-assembler     "xscmpeqqp" } } */
+/* { dg-final { scan-assembler     "xscmpgtqp" } } */
+/* { dg-final { scan-assembler     "xscmpgeqp" } } */
+/* { dg-final { scan-assembler     "xsmaxcqp"  } } */
+/* { dg-final { scan-assembler     "xsmincqp"  } } */
+/* { dg-final { scan-assembler     "xxsel"     } } */
+
+__float128
+f128_max1 (__float128 a, __float128 b)
+{
+  return (a >= b) ? a : b;
+}
+
+__float128
+f128_max2 (__float128 a, __float128 b)
+{
+  return (a > b) ? a : b;
+}
+
+__float128
+f128_min1 (__float128 a, __float128 b)
+{
+  return (a < b) ? a : b;
+}
+
+__float128
+f128_min2 (__float128 a, __float128 b)
+{
+  return (a <= b) ? a : b;
+}
+
+__float128
+f128_cmp_eq (__float128 a, __float128 b, __float128 c, __float128 d)
+{
+  return (a == b) ? c : d;
+}
+
+__float128
+f128_cmp_ne (__float128 a, __float128 b, __float128 c, __float128 d)
+{
+  return (a != b) ? c : d;
+}
+
+__float128
+f128_cmp_gt (__float128 a, __float128 b, __float128 c, __float128 d)
+{
+  return (a > b) ? c : d;
+}
+
+__float128
+f128_cmp_ge (__float128 a, __float128 b, __float128 c, __float128 d)
+{
+  return (a >= b) ? c : d;
+}
+
+__float128
+f128_cmp_lt (__float128 a, __float128 b, __float128 c, __float128 d)
+{
+  return (a < b) ? c : d;
+}
+
+__float128
+f128_cmp_le (__float128 a, __float128 b, __float128 c, __float128 d)
+{
+  return (a <= b) ? c : d;
+}
