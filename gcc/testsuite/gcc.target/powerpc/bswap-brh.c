0144a467da8d22c075488fa1626d419611870c1a
Michael Meissner <meissner@linux.ibm.com>
2020-06-23 11:05:58 -0400

    PowerPC: Add power10 BRD/BRW/BRH support.
    gcc/
    2020-06-23  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/rs6000.md (bswaphi2_reg): Generate the BRH
    	instruction on ISA 3.1.
    	(bswapsi2_reg): Generate the BRW instruction on ISA 3.1.
    	(bswapdi2): Rename bswapdi2_xxbrd to bswapdi2_brd.
    	(bswapdi2_brd): Rename from bswapdi2_xxbrd.  Generate the BRD
    	instruction on ISA 3.1.
    
    gcc/testsuite/
    2020-06-23  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/bswap-brd.c: New test.
    	* gcc.target/powerpc/bswap-brw.c: New test.
    	* gcc.target/powerpc/bswap-brh.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/bswap-brh.c
@@ -0,0 +1,12 @@
+/* { dg-do compile { target { powerpc*-*-* } } } */
+/* { dg-require-effective-target powerpc_power10_ok } */
+/* { dg-options "-O2 -mdejagnu-cpu=power10" } */
+
+/* This tests whether GCC generates the ISA 3.1 16-bit byte swap
+   instruction BRH.  */
+
+unsigned short
+bswap_short (unsigned short a)
+{
+  return __builtin_bswap16 (a); /* { dg-final { scan-assembler {\mbrh\M} } } */
+}
