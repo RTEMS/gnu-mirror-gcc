543629a0c9db2c0d6f47f99d7b75512555ea11ae
Andrew MacLeod <amacleod@redhat.com>
2020-08-26 13:31:26 -0400

    huh. missed the value-query files somehow

---
+++ gcc/value-query.cc
@@ -0,0 +1,122 @@
+/* Support routines for value queries
+   Copyright (C) 2019-2020 Free Software Foundation, Inc.
+   Major hacks by Aldy Hernandez <aldyh@redhat.com> and
+   Andrew MacLeod <amacleod@redhat.com>.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "backend.h"
+#include "tree.h"
+#include "gimple.h"
+#include "ssa.h"
+#include "tree-pretty-print.h"
+#include "fold-const.h"
+#include "value-range-equiv.h"
+#include "value-query.h"
+#include "alloc-pool.h"
+
+
+bool
+value_query::value_on_edge (tree &t, edge, tree name)
+{
+  return value_of_expr (t, name);
+}
+
+bool
+value_query::value_of_stmt (tree &t, gimple *, tree name)
+{
+  if (name)
+    return value_of_expr (t, name);
+  return false;
+}
+
+
+// -----------------------------------------------------------
+
+
+bool
+range_query::range_on_edge (irange &r, edge, tree name)
+{
+  return range_of_expr (r, name);
+}
+
+bool
+range_query::range_of_stmt (irange &r, gimple *, tree name)
+{
+  if (name)
+    return range_of_expr (r, name);
+  return false;
+}
+
+bool
+range_query::value_of_expr (tree &t, tree name, gimple *stmt)
+{
+  widest_irange r;
+  if (!irange::supports_type_p (TREE_TYPE (name)))
+    return false;
+  return (range_of_expr (r, name, stmt) && r.singleton_p (&t));
+}
+
+bool
+range_query::value_on_edge (tree &t, edge e, tree name)
+{
+  widest_irange r;
+  if (!irange::supports_type_p (TREE_TYPE (name)))
+    return false;
+  return (range_on_edge (r, e, name) && r.singleton_p (&t));
+}
+
+bool
+range_query::value_of_stmt (tree &t, gimple *stmt, tree name)
+{
+  widest_irange r;
+  if (!irange::supports_type_p (TREE_TYPE (name)))
+    return false;
+  return (range_of_stmt (r, stmt, name) && r.singleton_p (&t));
+}
+
+
+// valuation_query support routines for value_range_equiv's.
+
+class equiv_allocator : public object_allocator<value_range_equiv>
+{
+public:
+  equiv_allocator ()
+    : object_allocator<value_range_equiv> ("equiv_allocator pool") { }
+};
+
+const class value_range_equiv *
+range_query::get_value_range (const_tree expr, gimple *stmt)
+{
+  widest_irange r;
+  if (range_of_expr (r, const_cast<tree> (expr), stmt))
+    return new (equiv_pool->allocate ()) value_range_equiv (r);
+  return new (equiv_pool->allocate ()) value_range_equiv (TREE_TYPE (expr));
+}
+
+range_query::range_query ()
+{
+  equiv_pool = new equiv_allocator;
+}
+
+range_query::~range_query ()
+{
+  equiv_pool->release ();
+}
---
+++ gcc/value-query.h
@@ -0,0 +1,83 @@
+/* Support routines for value queries.
+   Copyright (C) 2019-2020 Free Software Foundation, Inc.
+   Contributed by Aldy Hernandez <aldyh@redhat.com> and
+   Andrew Macleod <amacleod@redhat.com>.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef GCC_QUERY_H
+#define GCC_QUERY_H
+
+
+// value_query is used by optimization passes that require valueizing ssa_names
+// in terms of a tree value, but have no neeed for ranges.
+//
+// value_of_expr is required to be provided.
+// value_on_edge and value_of_stmt default to simply calling value_of_expr.
+//
+// This implies the valuation is global in nature.  If a pass can make use
+// of more specific information, then it can override the other queries.
+//
+// Proper usage of the correct query in passes will enable other valuation
+// mechanisms to produce more precise results.
+
+class value_query
+{
+public:
+  virtual bool value_of_expr (tree &, tree name, gimple * = NULL) = 0;
+  virtual bool value_on_edge (tree &, edge, tree name);
+  virtual bool value_of_stmt (tree &, gimple *, tree name = NULL);
+};
+
+
+
+// range_query is used by optimization passes which are range aware.
+//
+// range_of_expr must be provided.  The default for range_on_edge and
+// range_of_stmt is to call range_of_expr.  If more precise results can
+// be calculated, those functions can be overridden.
+//
+// The default for the value_* routines is to call the equivalent range_*
+// routine, and check if the range is a singleton, and return that if so.
+//
+//  get_value_range is currently provided for compatibility with vr-values.
+//  It will be deprecated when possible.
+
+class range_query : public value_query
+{
+public:
+  range_query ();
+  virtual ~range_query ();
+
+  virtual bool value_of_expr (tree &, tree name, gimple * = NULL) OVERRIDE;
+  virtual bool value_on_edge (tree &, edge, tree name) OVERRIDE;
+  virtual bool value_of_stmt (tree &, gimple *, tree name = NULL) OVERRIDE;
+
+  virtual bool range_of_expr (irange &, tree name, gimple * = NULL) = 0;
+  virtual bool range_on_edge (irange &, edge, tree name);
+  virtual bool range_of_stmt (irange &, gimple *, tree name = NULL);
+
+  // DEPRECATED: This method is used from vr-values.  The plan is to
+  // rewrite all uses of it to the above API.
+  virtual const class value_range_equiv *get_value_range (const_tree,
+							  gimple * = NULL);
+
+private:
+  class equiv_allocator *equiv_pool;
+};
+
+#endif // GCC_QUERY_H
