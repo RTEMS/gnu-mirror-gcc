c76ddd4c67e6b7865149fc12c0c2b91826c9b2d1
Michael Meissner <meissner@linux.ibm.com>
2020-09-18 14:11:24 -0400

    PowerPC: Update __float128 <-> __ibm128 conversion.
    libgcc/
    2020-09-18  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/extendkftf2-sw.c: Move __float128 to __ibm128
    	conversion into float128-convert.h.
    	* config/rs6000/float128-convert.h: New file.
    	* config/rs6000/float128-hw.c: Move conversions between __float128
    	and __ibm128 into float128-convert.h.
    	* config/rs6000/quad-float128.h: Move conversions between
    	__float128 and __ibm128 into float128-convert.h.
    	* config/rs6000/trunctfkf2-sw.c: Move __ibm128 to __float128
    	conversion to float128-convert.h.

---
+++ libgcc/config/rs6000/float128-convert.h
@@ -0,0 +1,77 @@
+/* Convert between IEEE 128-bit and IBM 128-bit floating point types.
+   Copyright (C) 2016-2020 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Michael Meissner (meissner@linux.ibm.com).
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   In addition to the permissions in the GNU Lesser General Public
+   License, the Free Software Foundation gives you unlimited
+   permission to link the compiled version of this file into
+   combinations with other programs, and to distribute those
+   combinations without any restriction coming from the use of this
+   file.  (The Lesser General Public License restrictions do apply in
+   other respects; for example, they cover modification of the file,
+   and distribution when not linked into a combine executable.)
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+/* Implementation of conversions between __ibm128 and __float128, to allow the
+   same code to be used on systems with IEEE 128-bit emulation and with IEEE
+   128-bit hardware support.
+
+   These functions are called by the actual conversion functions called by the
+   compiler.  This code is here to allow being built at power8 (no hardware
+   float128) and power9 (hardware float128) varients that is selected by an
+   IFUNC function.  */
+
+static inline __ibm128 convert_float128_to_ibm128 (__float128);
+static inline __float128 convert_ibm128_to_float128 (__ibm128);
+
+static inline __ibm128
+convert_float128_to_ibm128 (__float128 value)
+{
+  double high, high_temp, low;
+
+  high = (double) value;
+  if (__builtin_isnan (high) || __builtin_isinf (high))
+    low = 0.0;
+
+  else
+    {
+      low = (double) (value - (__float128) high);
+      /* Renormalize low/high and move them into canonical IBM long
+	 double form.  */
+      high_temp = high + low;
+      low = (high - high_temp) + low;
+      high = high_temp;
+    }
+
+  return __builtin_pack_ibm128 (high, low);
+}
+
+static inline __float128
+convert_ibm128_to_float128 (__ibm128 value)
+{
+  double high = __builtin_unpack_ibm128 (value, 0);
+  double low = __builtin_unpack_ibm128 (value, 1);
+
+  /* Handle the special cases of NAN and infinity.  Similarly, if low is 0.0,
+     there no need to do the add.  In addition, avoiding the add produces the
+     correct sign if high is -0.0.  */
+  if (__builtin_isnan (high) || __builtin_isinf (high) || low == 0.0)
+    return (__float128) high;
+
+  else
+    return ((__float128) high) + ((__float128) low);
+}
