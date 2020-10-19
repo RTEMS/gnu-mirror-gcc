560a638ce5d63e25aeb0f94ba70089044915656c
Jan Hubicka <jh@suse.cz>
2020-10-19 13:45:38 +0200

    Fix ice with -fno-ipa-modref and icf
    2020-10-19  Jan Hubicka  <hubicka@ucw.cz>
    
    	* ipa-modref.c (modref_summaries::insert):
    	(modref_summaries_lto::insert):

--- gcc/ipa-modref.c
+++ gcc/ipa-modref.c
@@ -1137,7 +1137,10 @@ modref_summaries::insert (struct cgraph_node *node, modref_summary *)
       optimization_summaries->remove (node);
       return;
     }
-  if (!DECL_STRUCT_FUNCTION (node->decl))
+  /* If funtion is not in giple form yet or the pass is disabled, make
+     sure we do not keep stale summary.  */
+  if (!DECL_STRUCT_FUNCTION (node->decl)
+      || !opt_for_fn (node->decl, flag_ipa_modref))
     {
       summaries->remove (node);
       return;
@@ -1156,7 +1159,8 @@ modref_summaries_lto::insert (struct cgraph_node *node, modref_summary_lto *)
      propagated.  This is done only by SIMD cloning that is not very
      critical.  */
   if (!DECL_STRUCT_FUNCTION (node->decl)
-      || propagated)
+      || propagated
+      || !opt_for_fn (node->decl, flag_ipa_modref))
     {
       summaries_lto->remove (node);
       return;
