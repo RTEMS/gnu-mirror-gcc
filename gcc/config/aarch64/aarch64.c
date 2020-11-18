b0827115e53c6c5097f70ff7c7cdf2ca320e50a9
Sebastian Pop <spop@amazon.com>
2020-11-18 19:36:35 +0000

    aarch64: Extend %R for integer registers
    build failed when configured with --with-tune=neoverse-n1
    libsanitizer/tsan/tsan_interface_atomic.cc:426:1: internal compiler error: output_operand: incompatible floating point / vector register operand for '%R'
    
    This patch fixes backported patch 3d05e960c18653ce1e4f19fba701645b1a030da7:
    the backported patch incorrectly applied to a different section of the file
    than %R registers.

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -5167,13 +5167,12 @@ aarch64_print_operand (FILE *f, rtx x, int code)
     case 'd':
     case 'q':
       /* Print a scalar FP/SIMD register name.  */
-      if (REG_P (x) && FP_REGNUM_P (REGNO (x)))
-	asm_fprintf (f, "%c%d", code, REGNO (x) - V0_REGNUM);
-      else if (REG_P (x) && GP_REGNUM_P (REGNO (x)))
-	asm_fprintf (f, "%c%d", code, REGNO (x) - R0_REGNUM);
-      else
-	output_operand_lossage ("incompatible register operand for '%%%c'",
-				code);
+      if (!REG_P (x) || !FP_REGNUM_P (REGNO (x)))
+       {
+         output_operand_lossage ("incompatible floating point / vector register operand for '%%%c'", code);
+         return;
+       }
+      asm_fprintf (f, "%c%d", code, REGNO (x) - V0_REGNUM);
       break;
 
     case 'S':
@@ -5191,12 +5190,13 @@ aarch64_print_operand (FILE *f, rtx x, int code)
 
     case 'R':
       /* Print a scalar FP/SIMD register name + 1.  */
-      if (!REG_P (x) || !FP_REGNUM_P (REGNO (x)))
-	{
-	  output_operand_lossage ("incompatible floating point / vector register operand for '%%%c'", code);
-	  return;
-	}
-      asm_fprintf (f, "q%d", REGNO (x) - V0_REGNUM + 1);
+      if (REG_P (x) && FP_REGNUM_P (REGNO (x)))
+        asm_fprintf (f, "q%d", REGNO (x) - V0_REGNUM + 1);
+      else if (REG_P (x) && GP_REGNUM_P (REGNO (x)))
+        asm_fprintf (f, "x%d", REGNO (x) - R0_REGNUM + 1);
+      else
+        output_operand_lossage ("incompatible register operand for '%%%c'",
+                                code);
       break;
 
     case 'X':
