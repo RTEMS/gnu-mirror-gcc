8e62a84a344ccb6d60de75ab891b675c6970cde2
H.J. Lu <hjl.tools@gmail.com>
2020-12-02 05:32:37 -0800

    Backport Use the section flag 'o' for __patchable_function_entries
    This commit in GNU binutils 2.35:
    
    https://sourceware.org/git/gitweb.cgi?p=binutils-gdb.git;a=commit;h=b7d072167715829eed0622616f6ae0182900de3e
    
    added the section flag 'o' to .section directive:
    
    .section __patchable_function_entries,"awo",@progbits,foo
    
    which specifies the symbol name which the section references.  Assembler
    creates a unique __patchable_function_entries section with the section,
    where foo is defined, as its linked-to section.  Linker keeps a section
    if its linked-to section is kept during garbage collection.
    
    This patch checks assembler support for the section flag 'o' and uses
    it to implement __patchable_function_entries section.  Since Solaris may
    use GNU assembler with Solairs ld.  Even if GNU assembler supports the
    section flag 'o', it doesn't mean that Solairs ld supports it.  This
    feature is disabled for Solairs targets.
    
    gcc/
    
    	PR middle-end/93195
    	PR middle-end/93197
    	* configure.ac (HAVE_GAS_SECTION_LINK_ORDER): New.  Define 1 if
    	the assembler supports the section flag 'o' for specifying
    	section with link-order.
    	* output.h (SECTION_LINK_ORDER): New.  Defined to 0x8000000.
    	(SECTION_MACH_DEP): Changed from 0x8000000 to 0x10000000.
    	* targhooks.c (default_print_patchable_function_entry): Pass
    	SECTION_LINK_ORDER to switch_to_section if the section flag 'o'
    	works.  Pass current_function_decl to switch_to_section.
    	* varasm.c (default_elf_asm_named_section): Use 'o' flag for
    	SECTION_LINK_ORDER if assembler supports it.
    	* config.in: Regenerated.
    	* configure: Likewise.
    	* doc/sourcebuild.texi: Document o_flag_in_section.
    
    gcc/testsuite/
    
    	PR middle-end/93195
    	* g++.dg/pr93195b.C: New test.
    	* lib/target-supports.exp
    	(check_effective_target_o_flag_in_section): New proc.

--- gcc/varasm.c
+++ gcc/varasm.c
@@ -6398,6 +6398,10 @@ default_elf_asm_named_section (const char *name, unsigned int flags,
 	*f++ = TLS_SECTION_ASM_FLAG;
       if (HAVE_COMDAT_GROUP && (flags & SECTION_LINKONCE))
 	*f++ = 'G';
+      if (flags & SECTION_RETAIN)
+	*f++ = 'R';
+      if (flags & SECTION_LINK_ORDER)
+	*f++ = 'o';
 #ifdef MACH_DEP_SECTION_ASM_FLAG
       if (flags & SECTION_MACH_DEP)
 	*f++ = MACH_DEP_SECTION_ASM_FLAG;
@@ -6426,6 +6430,14 @@ default_elf_asm_named_section (const char *name, unsigned int flags,
 
       if (flags & SECTION_ENTSIZE)
 	fprintf (asm_out_file, ",%d", flags & SECTION_ENTSIZE);
+      if (flags & SECTION_LINK_ORDER)
+	{
+	  tree id = DECL_ASSEMBLER_NAME (decl);
+	  ultimate_transparent_alias_target (&id);
+	  const char *name = IDENTIFIER_POINTER (id);
+	  name = targetm.strip_name_encoding (name);
+	  fprintf (asm_out_file, ",%s", name);
+	}
       if (HAVE_COMDAT_GROUP && (flags & SECTION_LINKONCE))
 	{
 	  if (TREE_CODE (decl) == IDENTIFIER_NODE)
