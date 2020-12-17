1aa62ff911040e4aae291ddfcc3a5436e45302de
Michael Meissner <meissner@linux.ibm.com>
2020-12-17 00:47:36 -0500

    PowerPC: Add float128/Decimal conversions.
    This patch replaces the following two patches:
    
    September 24th, 2020:
    Message-ID: <20200924203545.GD31597@ibm-toto.the-meissners.org>
    
    October 22nd, 2020:
    Message-ID: <20201022220603.GA11658@ibm-toto.the-meissners.org>
    
    This is a simplification of those patches.  Those patches were initially
    written before I was using the final glibc 2.32 (Advance Toolchain AT14.0).
    With using that glibc and with the previous IEEE patches submitted, I can
    simplify the conversions to just use the long double defaults, compiling the
    modules for IEEE 128-bit long double.  It works because stdio.h/gcc switches
    the sprintf call to __sprintfieee128, and the strtold call to __strtof128.
    
    While most of the Decimal <-> Long double tests now pass when long doubles are
    IEEE 128-bit, there is one test that fails:
    
    	c-c++-common/dfp/convert-bfp-11.c
    
    This test explicitly expects long double to be IBM 128-bit extended double.  A
    later patch will fix this.
    
    If the glibc is not 2.32 or later, this code just compiles to using abort.
    That way the user won't get unknown reference errors due to the calls to the
    glibc 2.32 functions that aren't in previous glibcs.
    
    This patch is one of three critical patches needed to be able to build
    compilers where the default is IEEE 128-bit.  The other patches were the
    patches to rename the built-in functions, and the patches for prs 97543 and
    97643 that were both posted earlier.
    
    libgcc/
    2020-12-17  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/t-float128 (fp128_dec_funcs): New macro.
    	(ibm128_dec_funcs): New macro.
    	(fp128_ppc_funcs): Add the Decimal <-> __float128 conversions.
    	(fp128_dec_objs): Force Decimal <-> __float128 conversions to be
    	compiled with -mabi=ieeelongdouble.
    	(ibm128_dec_objs): Force Decimal <-> __float128 conversions to be
    	compiled with -mabi=ieeelongdouble.
    	(FP128_CFLAGS_DECIMAL): New macro.
    	(IBM128_CFLAGS_DECIMAL): New macro.
    	* config/rs6000/_dd_to_kf.c: New file.
    	* config/rs6000/_kf_to_dd.c: New file.
    	* config/rs6000/_kf_to_sd.c: New file.
    	* config/rs6000/_kf_to_td.c: New file.
    	* config/rs6000/_sd_to_kf.c: New file.
    	* config/rs6000/_td_to_kf.c: New file.

---
+++ libgcc/config/rs6000/_dd_to_kf.c
@@ -0,0 +1,58 @@
+/* Copyright (C) 1989-2020 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+Under Section 7 of GPL version 3, you are granted additional
+permissions described in the GCC Runtime Library Exception, version
+3.1, as published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License and
+a copy of the GCC Runtime Library Exception along with this program;
+see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+<http://www.gnu.org/licenses/>.  */
+
+/* Decimal64 -> _Float128 conversion.  */
+
+/* FINE_GRAINED_LIBRARIES is used so we can isolate just to dd_to_tf conversion
+   function from dp-bits.c.  */
+#define FINE_GRAINED_LIBRARIES	1
+#define L_dd_to_tf		1
+#define WIDTH			64
+
+#if !defined(__LONG_DOUBLE_128__) || !defined(__LONG_DOUBLE_IEEE128__)
+#error "Long double is not IEEE 128-bit"
+#endif
+
+/* If we don't have at least GLIBC 2.32, the strtold used by dfp-bit.c does not
+   have support for float128.  Add an abort in case this is called.  */
+#include <features.h>
+
+#if ((__GLIBC__ * 1000) + __GLIBC_MINOR__) < 2032
+
+#include <stdlib.h>
+extern long double __dpd_extendddkf (_Decimal64);
+
+long double
+__dpd_extendddkf (_Decimal64 x __attribute__ ((unused)))
+{
+  abort ();
+}
+
+#else
+
+/* Map the external name to the float128 default.  */
+#define __dpd_extendddtf	__dpd_extendddkf
+
+/* Use dfp-bit.c to do the real work.  */
+#include "dfp-bit.c"
+#endif
