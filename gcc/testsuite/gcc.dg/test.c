548996a6fa5659d1db6750a269fc4f456f28c6d6
Martin Liska <mliska@suse.cz>
2020-12-21 13:35:14 +0100

    Test by Tom.

---
+++ gcc/testsuite/gcc.dg/test.c
@@ -0,0 +1,6 @@
+int
+main (void)
+{
+  return 0;
+}
+/* { dg-final { pytest-execute /home/marxin/Programming/gcc/gcc/testsuite/gcc.dg/test.py } } */
---
+++ gcc/testsuite/gcc.dg/test.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_func():
+    pass
+
+def test2():
+    assert 1 == 2
