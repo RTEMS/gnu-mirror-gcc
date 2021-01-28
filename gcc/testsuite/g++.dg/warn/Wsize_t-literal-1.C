7741245f1199a9ef01807aa72a38f89e52937a7b
Ed Smith-Rowland <3dw4rd@verizon.net>
2021-01-28 16:52:35 -0500

    More test tweaks.
    Rename from ptrdiff to size_t.

---
+++ gcc/testsuite/g++.dg/warn/Wsize_t-literal-1.C
@@ -0,0 +1,11 @@
+// { dg-do compile { target c++20_down } }
+
+#include <cstddef>
+
+std::size_t s1 = 1234zu; // { dg-warning "use of C\+\+23 size_t integer constant" "" { target c++20_down } }
+std::size_t S1 = 5678ZU; // { dg-warning "use of C\+\+23 size_t integer constant" "" { target c++20_down } }
+std::size_t s2 = 1234uz; // { dg-warning "use of C\+\+23 size_t integer constant" "" { target c++20_down } }
+std::size_t S2 = 5678UZ; // { dg-warning "use of C\+\+23 size_t integer constant" "" { target c++20_down } }
+
+std::ptrdiff_t pd1 = 1234z; // { dg-warning "use of C\+\+23 ptrdiff_t integer constant" "" { target c++20_down } }
+std::ptrdiff_t PD1 = 5678Z; // { dg-warning "use of C\+\+23 ptrdiff_t integer constant" "" { target c++20_down } }
--- gcc/testsuite/gcc.dg/Wsize_t-literal-2.c
+++ gcc/testsuite/gcc.dg/Wsize_t-literal-2.c
@@ -4,6 +4,9 @@
 #include <stddef.h>
 
 size_t s1 = 1234zu;
+size_t S1 = 5678ZU;
 size_t s2 = 1234uz;
+size_t S2 = 5678UZ;
 
 ptrdiff_t pd1 = 1234z;
+ptrdiff_t PD1 = 5678Z;
