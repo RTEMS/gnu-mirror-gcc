b5cab3f7bb617174dcf842bd1dfe1b995b195b70
Michael Meissner <meissner@linux.ibm.com>
2021-02-17 17:35:17 -0500

    Optimize V4SI/V4SF constants on PowerPC.
    This patch reworks the XXSPLTIW support to use vec_duplicate instead of UNSPEC.
    It also changes the vector initialization code to generate XXSPLITW when we
    can.
    
    gcc/
    2021-02-17  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/altivec.md (UNSPEC_XXSPLTIW) Delete.
    	(xxspltiw_v4si): Rewrite to use vec_duplicate.  Add optimizations
    	to generate VSPLTIB or XXSPLTIB if possible.
    	(xxspltiw_v4sf): Rewrite to use vec_duplicate.  Add optimizations
    	to generate VSPLTIB or XXSPLTIB if possible.
    	(xxspltiw_v4sf_inst): Delete, no longer used.
    	* config/rs6000/predicates.md (easy_vector_constant): Recognize
    	vector constants that we can load with XXSPLTIW.
    	* config/rs6000/rs6000-protos.h (xxspltiw_constant_p): Add
    	declaration.
    	* config/rs6000/rs6000.c (xxspltiw_constant_p): New function.
    	(xxspltib_constant_p): If xxsplitw_constant_p returns true, don't
    	return true to use xxspltib and extend.
    	(output_vec_const_move): Add support to generate XXSPLITW if
    	possible.
    	(rs6000_expand_vector_init): Generate XXSPLITW instructions if we
    	can.
    
    gcc/testsuite/
    2021-02-17  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/xxspltiw-1.c: New test.
    	* gcc.target/powerpc/xxspltiw-2.c: New test.
    	* gcc.target/powerpc/xxspltiw-3.c: New test.
    	* gcc.target/powerpc/xxspltiw-4.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/xxspltiw-1.c
@@ -0,0 +1,56 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target power10_ok } */
+/* { dg-options "-O2 -mdejagnu-cpu=power10" } */
+
+/* Test that XXSPLTIW is generated to load V4SI constants.  */
+vector int
+init_0 (void)
+{
+  return (vector int) { 0, 0, 0, 0 };			/* vspltisw.  */
+}
+
+vector int
+init_m1 (void)
+{
+  return (vector int) { -1, -1, -1, -1 };		/* vspltisw.  */
+}
+
+vector int
+init_3 (void)
+{
+  return (vector int) { 3, 3, 3, 3 };			/* vspltisw.  */
+}
+
+vector int
+init_23 (void)
+{
+  return (vector int) { 23, 23, 23, 23 };		/* xxspltisw.  */
+}
+
+vector int
+init_1023 (void)
+{
+  return (vector int) { 1023, 1023, 1023, 1023 };	/* xxspltisw.  */
+}
+
+/* Test using xxsplitb to load 0 in FPR registers.  */
+void
+init_0_fpr (vector int *p)
+{
+  vector int v = (vector int) { 0, 0, 0, 0 };		/* xxspltisb.  */
+  __asm__ ("# %x0" : "+f" (v));
+  *p = v;
+}
+
+/* Test using xxsplitb to load -1 in FPR registers.  */
+void
+init_m1_fpr (vector int *p)
+{
+  vector int v = (vector int) { -1, -1, -1, -1 };	/* xxspltisb.  */
+  __asm__ ("# %x0" : "+f" (v));
+  *p = v;
+}
+
+/* { dg-final { scan-assembler-times {\mvspltisw|xxspltib\M} 5 } } */
+/* { dg-final { scan-assembler-times {\mxxspltiw\M}          2 } } */
+/* { dg-final { scan-assembler-not   {\mvextsb2w\M}            } } */
