26353bda77b000a1f8357d3d0264e0517f04329b
Michael Meissner <meissner@linux.ibm.com>
2021-02-17 02:29:45 -0500

    Generate power10 XXSPLTIW instruction for vectors.
    gcc/
    2021-02-17  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/rs6000.c (vspltis_constant): Treat constants
    	loaded with XXSPLTIW as valid for easy altivec constants.
    	(xxspltib_constant_p): If we have XXSPLTIW, for V4SI do not use
    	XXSPLTIB/VEXTSB2W to load V4SI constants.
    	(output_vec_const_move): Generate XXSPLTIW.
    	* config/rs6000/vsx.md (vsx_splat_v4si_constant): New insn.
    
    gcc/testsuite/
    2021-02-17  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/xxspltiw-1.c: New test.
    	* gcc.target/powerpc/xxspltiw-2.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/xxspltiw-2.c
@@ -0,0 +1,28 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target power10_ok } */
+/* { dg-options "-O2 -mdejagnu-cpu=power10" } */
+
+/* Test that XXSPLTIW is generated to load V4SI constants.  */
+#include <altivec.h>
+
+vector int
+splat_3 (void)
+{
+  return vec_splats ((int)3);		/* vspltisw.  */
+}
+
+vector int
+splat_23 (void)
+{
+  return vec_splats ((int)23);		/* xxspltisw.  */
+}
+
+vector int
+splat_1023 (void)
+{
+  return vec_splats ((int)1023);	/* xxspltisw.  */
+}
+
+/* { dg-final { scan-assembler-times {\mvspltisw\M} 1 } } */
+/* { dg-final { scan-assembler-times {\mxxspltiw\M} 2 } } */
+/* { dg-final { scan-assembler-not   {\mxxspltib\M}   } } */
