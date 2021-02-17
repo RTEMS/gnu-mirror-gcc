5151fc3a3e9b04989344796b386a14e1810520a9
Michael Meissner <meissner@linux.ibm.com>
2021-02-17 02:39:22 -0500

    Warn about %m or %o constraints in power10 asms (PR 98519).
    gcc/
    2021-02-17  Michael Meissner  <meissner@linux.ibm.com>
    
    	PR target/98519
    	* config/rs6000/rs6000.c (rs6000_md_asm_adjust): If prefixed
    	addresses are allowed, warn about using the 'm' and 'o'
    	constraints.
    
    gcc/testsuite/
    2021-02-17  Michael Meissner  <meissner@linux.ibm.com>
    
    	PR target/98519
    	* gcc.target/powerpc/pr98519.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/pr98519.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target powerpc_prefixed_addr } */
+/* { dg-options "-O2 -mdejagnu-cpu=power10" } */
+
+static int x;
+
+int
+get (void)
+{
+  int a;
+  __asm__ ("lwz%U1%X1 %0,%1" : "=r" (a) : "m" (x)); /* { dg-warning "Asm constraint" } */
+  return a;
+}
+
+void
+put (int a)
+{
+  __asm__ ("stw%U0%X0 %1,%0" : "=m" (x) : "r" (a)); /* { dg-warning "Asm constraint" } */
+  return;
+}
