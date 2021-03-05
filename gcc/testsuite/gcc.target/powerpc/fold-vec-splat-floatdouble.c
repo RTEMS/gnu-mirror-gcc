30c0bb52937160fb02ea7297dce875d58770a55c
Bill Schmidt <wschmidt@linux.ibm.com>
2021-03-05 17:10:15 -0600

    rs6000: Test case adjustments
    2021-03-05  Bill Schmidt  <wschmidt@linux.ibm.com>
    
    gcc/testsuite/
    	* gcc.target/powerpc/bfp/scalar-test-neg-2.c: Adjust.
    	* gcc.target/powerpc/bfp/scalar-test-neg-3.c: Adjust.
    	* gcc.target/powerpc/bfp/scalar-test-neg-5.c: Adjust.
    	* gcc.target/powerpc/cmpb-2.c: Adjust.
    	* gcc.target/powerpc/cmpb32-2.c: Adjust.
    	* gcc.target/powerpc/crypto-builtin-2.c: Adjust.
    	* gcc.target/powerpc/fold-vec-splat-floatdouble.c: Adjust.
    	* gcc.target/powerpc/fold-vec-splat-longlong.c: Adjust.
    	* gcc.target/powerpc/fold-vec-splat-misc-invalid.c: Adjust.
    	* gcc.target/powerpc/p8vector-builtin-8.c: Adjust.
    	* gcc.target/powerpc/pr80315-1.c: Adjust.
    	* gcc.target/powerpc/pr80315-2.c: Adjust.
    	* gcc.target/powerpc/pr80315-3.c: Adjust.
    	* gcc.target/powerpc/pr80315-4.c: Adjust.
    	* gcc.target/powerpc/pr88100.c: Adjust.
    	* gcc.target/powerpc/pragma_misc9.c: Adjust.
    	* gcc.target/powerpc/pragma_power8.c: Adjust.
    	* gcc.target/powerpc/pragma_power9.c: Adjust.
    	* gcc.target/powerpc/test_fpscr_drn_builtin_error.c: Adjust.
    	* gcc.target/powerpc/test_fpscr_rn_builtin_error.c: Adjust.
    	* gcc.target/powerpc/vec-gnb-2.c: Adjust.
    	* gcc.target/powerpc/vsu/vec-all-nez-7.c: Adjust.
    	* gcc.target/powerpc/vsu/vec-any-eqz-7.c: Adjust.
    	* gcc.target/powerpc/vsu/vec-cmpnez-7.c: Adjust.
    	* gcc.target/powerpc/vsu/vec-cntlz-lsbb-2.c: Adjust.
    	* gcc.target/powerpc/vsu/vec-cnttz-lsbb-2.c: Adjust.
    	* gcc.target/powerpc/vsu/vec-xst-len-12.c: Adjust.

--- gcc/testsuite/gcc.target/powerpc/fold-vec-splat-floatdouble.c
+++ gcc/testsuite/gcc.target/powerpc/fold-vec-splat-floatdouble.c
@@ -18,7 +18,7 @@ vector float test_fc ()
 vector double testd_00 (vector double x) { return vec_splat (x, 0b00000); }
 vector double testd_01 (vector double x) { return vec_splat (x, 0b00001); }
 vector double test_dc ()
-{ const vector double y = { 3.0, 5.0 }; return vec_splat (y, 0b00010); }
+{ const vector double y = { 3.0, 5.0 }; return vec_splat (y, 0b00001); }
 
 /* If the source vector is a known constant, we will generate a load.  */
 /* { dg-final { scan-assembler-times {\mlvx\M|\mlxvd2x\M|\mlxv\M} 2 } } */
@@ -27,5 +27,5 @@ vector double test_dc ()
 /* { dg-final { scan-assembler-times "vspltw|xxspltw" 3 } } */
 
 /* For double types, we will generate xxpermdi instructions.  */
-/* { dg-final { scan-assembler-times "xxpermdi" 3 } } */
+/* { dg-final { scan-assembler-times "xxpermdi" 2 } } */
 
