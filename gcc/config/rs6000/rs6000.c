51af0b97ddb5a147418eebddb054f4ba5e4c9691
Michael Meissner <meissner@linux.ibm.com>
2021-04-07 16:22:54 -0400

    Implement XXSPLTIDP support.
    This patch implements XXSPLTIDP support for SF and DF scalar constants and
    V2DF vector constants.
    
    A new constraint (eF) is added to match constants that can be loaded with
    the XXSPLTIDP instruction.
    
    I have moved the XXSPLTIDP built-in function support from altivec.md to
    vsx.md because the functions can load any VSX register, not just the
    ALTIVEC registers.
    
    I have added a temporary switch (-mxxspltidp) to control whether or not the
    XXSPLTIDP instruction is generated.
    
    This patch provides a xxspltidp_constant_p function which decodes both
    VEC_DUPLICATE and VECTOR_CONST insns (similar to the existing
    xxspltib_constant_p function).
    
    The xxspltidp_constant_p function returns the appropriate integer that will be
    used in the XXSPLTIDP instruction.  Note, because SFmode denormal values
    are undefined in the hardware, the xxspltidp_constant_p function returns
    false.  Also xxspltidp_constant_p returns false for 0.0 because is cheaper
    to implement without XXSPLTIDP.
    
    gcc/
    2021-04-07  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/altivec.md (UNSPEC_XXSPLTID): Move to vsx.md and
    	rename to UNSPEC_XXSPLTID.
    	(xxspltidp_v2df): Move to vsx.md and re-implement.
    	(xxspltidp_v2df_inst): Move to vsx.md and re-implement.
    	* config/rs6000/constraints.md (eF): New constraint.
    	* config/rs6000/predicates.md (easy_fp_constant): If we load the
    	scalar constant with XXSPLTIDP, return true.
    	(xxspltidp_operand): New predicate.
    	(easy_vector_constant): If we can generate XXSPLTIDP, mark the
    	vector constant as easy.
    	* config/rs6000/rs6000-cpus.def (OTHER_POWER10_MASKS): Add
    	-mxxspltidp support.
    	(POWERPC_MASKS): Add -mxxspltidp support.
    	* config/rs6000/rs6000-protos.h (xxspltidp_constant_p): New
    	declaration.
    	* config/rs6000/rs6000.c (rs6000_option_override_internal): Add
    	-mxxspltidp support.
    	(xxspltidp_constant_p): New function.
    	(output_vec_const_move): Add support for XXSPLTIDP.
    	(rs6000_opt_masks): Add -mxxspltidp support.
    	(rs6000_emit_xxspltidp_v2df): Change function to implement the
    	XXSPLTIDP instruction.
    	* config/rs6000/rs6000.md (movsf_hardfloat): Add XXSPLTIDP
    	support.
    	(mov<mode>_hardfloat32, FMOVE64 iterator): Add XXSPLTIDP support.
    	(mov<mode>_hardfloat64, FMOVE64 iterator): Add XXSPLTIDP support.
    	* config/rs6000/rs6000.opt (-mxxspltidp): New switch.
    	* config/rs6000/vsx.md (UNSPEC_XXSPLTIDP): Move here from
    	altivec.md.  Rename it to UNSPEC_XXSPLTIDP to match the
    	instruction.
    	(vsx_mov<mode>_64bit): Add XXSPLTIDP support.
    	(vsx_mov<mode>_32bit): Add XXSPLTIDP support.
    	(XXSPLTIDP): New mode iterator.
    	(xxspltidp_<mode>_internal1): New define_insn_and_split.
    	(xxspltidp_<mode>_internal2): New define_insn.
    	(xxspltidp_v2df): Move to vsx.md from altivec.md.  Re-implement to
    	use the new constant format.

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -4480,6 +4480,10 @@ rs6000_option_override_internal (bool global_init_p)
       && (rs6000_isa_flags_explicit & OPTION_MASK_XXSPLTIW) == 0)
     rs6000_isa_flags |= OPTION_MASK_XXSPLTIW;
 
+  if (TARGET_POWER10 && TARGET_VSX
+      && (rs6000_isa_flags_explicit & OPTION_MASK_XXSPLTIDP) == 0)
+    rs6000_isa_flags |= OPTION_MASK_XXSPLTIDP;
+
   if (!TARGET_PCREL && TARGET_PCREL_OPT)
     rs6000_isa_flags &= ~OPTION_MASK_PCREL_OPT;
 
@@ -6537,6 +6541,75 @@ xxspltiw_constant_p (rtx op,
   return true;
 }
 
+/* Return true if OP is of the given MODE and can be synthesized with ISA 3.1
+   XXSPLTIDP instruction.
+
+   Return the constant that is being split via CONSTANT_PTR to use in the
+   XXSPLTIDP instruction.  */
+
+bool
+xxspltidp_constant_p (rtx op,
+		      machine_mode mode,
+		      HOST_WIDE_INT *constant_ptr)
+{
+  *constant_ptr = 0;
+
+  if (!TARGET_XXSPLTIDP)
+    return false;
+
+  if (mode == VOIDmode)
+    mode = GET_MODE (op);
+
+  rtx element = op;
+  if (mode == V2DFmode)
+    {
+      /* Handle VEC_DUPLICATE and CONST_VECTOR.  */
+      if (GET_CODE (op) == VEC_DUPLICATE)
+       element = XEXP (op, 0);
+
+      else if (GET_CODE (op) == CONST_VECTOR)
+       {
+         element = CONST_VECTOR_ELT (op, 0);
+         if (!rtx_equal_p (element, CONST_VECTOR_ELT (op, 1)))
+           return false;
+       }
+
+      else
+       return false;
+
+      mode = DFmode;
+    }
+
+  if (mode != SFmode && mode != DFmode)
+    return false;
+
+  if (GET_MODE (element) != mode)
+    return false;
+
+  if (!CONST_DOUBLE_P (element))
+    return false;
+
+  /* Don't return true for 0.0 since that is easy to create without
+     XXSPLTIDP.  */
+  if (element == CONST0_RTX (mode))
+    return false;
+
+  /* If the value doesn't fit in a SFmode, exactly, we can't use XXSPLTIDP.  */
+  const struct real_value *rv = CONST_DOUBLE_REAL_VALUE (element);
+  if (!exact_real_truncate (SFmode, rv))
+    return 0;
+
+  long value;
+  REAL_VALUE_TO_TARGET_SINGLE (*rv, value);
+
+  /* Test for SFmode denormal (exponent is 0, mantissa field is non-zero).  */
+  if (((value & 0x7F800000) == 0) && ((value & 0x7FFFFF) != 0))
+    return false;
+
+  *constant_ptr = value;
+  return true;
+}
+
 const char *
 output_vec_const_move (rtx *operands)
 {
@@ -6601,6 +6674,13 @@ output_vec_const_move (rtx *operands)
 	    return "vspltisw %0,%2";
 
 	  return "xxspltiw %x0,%2";
+	}
+
+      HOST_WIDE_INT xxspltidp_value = 0;
+      if (xxspltidp_constant_p (vec, mode, &xxspltidp_value))
+	{
+	  operands[2] = GEN_INT (xxspltidp_value);
+	  return "xxspltidp %x0,%2";
 	}
 
       if (TARGET_P9_VECTOR
@@ -24101,6 +24181,7 @@ static struct rs6000_opt_mask const rs6000_opt_masks[] =
   { "update",			OPTION_MASK_NO_UPDATE,		true , true  },
   { "vsx",			OPTION_MASK_VSX,		false, true  },
   { "xxspltiw",			OPTION_MASK_XXSPLTIW,		false, true  },
+  { "xxspltidp",		OPTION_MASK_XXSPLTIDP,		false, true  },
 #ifdef OPTION_MASK_64BIT
 #if TARGET_AIX_OS
   { "aix64",			OPTION_MASK_64BIT,		false, false },
@@ -27926,7 +28007,7 @@ rs6000_emit_xxspltidp_v2df (rtx dst, long value)
     inform (input_location,
 	    "the result for the xxspltidp instruction "
 	    "is undefined for subnormal input values");
-  emit_insn( gen_xxspltidp_v2df_inst (dst, GEN_INT (value)));
+  emit_insn( gen_xxspltidp_v2df_internal2 (dst, GEN_INT (value)));
 }
 
 /* Implement TARGET_ASM_GENERATE_PIC_ADDR_DIFF_VEC.  */
