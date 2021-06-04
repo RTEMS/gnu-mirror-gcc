ac51d8244f28d7c94edb5ee5595a62cd9de9ea7d
Michael Meissner <meissner@linux.ibm.com>
2021-06-03 21:25:35 -0400

    Generate LXVKQ on power10.
    This patch generates the LXVKQ instruction to load certain IEEE 128-bit
    constants.
    
    gcc/
    2021-06-03  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/constraint.md (eQ): New constraint.
    	* config/rs6000/predicates.md (easy_fp_constant): If the constant
    	can be loaded with LXVKQ, it is easy.
    	(lxvkq_operand): New predicate.
    	* config/rs6000/rs6000-protos.h (lxvkq_constant_p): New
    	declaration.
    	* config/rs6000/rs6000-cpus.h (ISA_3_1_MASKS_SERVER): Add -mlxvkq.
    	(POWERPC_MASKS): Add -mlxvkq.
    	* config/rs6000/rs6000.c (rs6000_option_override_internal): Add
    	support for -mlxvkq.
    	(lxvkq_constant_p): New function.
    	(output_vec_const_move): Add support for generating lxvkq.
    	(rs6000_output_move_128bit): Add support for generating lxvkq.
    	(rs6000_opt_masks): Add -mlxvkq.
    	* config/rs6000/rs6000.opt (-mlxvkq): New option.
    	* config/rs6000/vsx.md (vsx_mov<mode>_64bit): Add support to
    	generate lxvkq.
    	(vsx_mov<mode>_32bit): Add support to generate lxvkq.
    
    gcc/testsuite/
    2021-06-03  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/float128-constant.c: New test.

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -4511,9 +4511,13 @@ rs6000_option_override_internal (bool global_init_p)
 
       if ((rs6000_isa_flags_explicit & OPTION_MASK_XXSPLTIDP) == 0)
 	rs6000_isa_flags |= OPTION_MASK_XXSPLTIDP;
+
+      if ((rs6000_isa_flags_explicit & OPTION_MASK_LXVKQ) == 0)
+	rs6000_isa_flags |= OPTION_MASK_LXVKQ;
     }
   else
-    rs6000_isa_flags &= ~(OPTION_MASK_XXSPLTIW
+    rs6000_isa_flags &= ~(OPTION_MASK_LXVKQ
+			  | OPTION_MASK_XXSPLTIW
 			  | OPTION_MASK_XXSPLTIDP
 			  | OPTION_MASK_XXSPLTI32DX);
 
@@ -6722,6 +6726,86 @@ xxsplti32dx_constant_p (rtx op,
   return false;
 }
 
+/* Return true if OP is of the given MODE is one of the 18 special values that
+   can be generated with the LXVKQ instruction.
+
+   Return the constant that will go in the LXVKQ instruction.
+
+   The LXVKQ immediates are:
+	1 - 7:		1.0 .. 7.0.
+	8:		Positive infinity.
+	9:		Default quiet NaN.
+	16:		-0.0.
+	17 - 23:	-1.0 .. 7.0.
+	24:		Negative infinity.  */
+
+bool
+lxvkq_constant_p (rtx op,
+		  machine_mode mode,
+		  int *imm_p)
+{
+  *imm_p = -1;
+
+  if (!TARGET_LXVKQ)
+    return false;
+
+  if (mode == VOIDmode)
+    mode = GET_MODE (op);
+
+  if (!FLOAT128_IEEE_P (mode))
+    return false;
+
+  if (!CONST_DOUBLE_P (op))
+    return false;
+
+  /* All of the values generated can be expressed as SFmode values, so if it
+     doesn't fit in SFmode, exit.  */
+  const struct real_value *rv = CONST_DOUBLE_REAL_VALUE (op);
+  if (!exact_real_truncate (SFmode, rv))
+    return 0;
+
+  /* +/- Inifinity  is 8/24.  */
+  if (REAL_VALUE_ISINF (*rv))
+    {
+      *imm_p = real_isneg (rv) ? 24 : 8;
+      return true;
+    }
+
+  /* NaN is 9.  */
+  if (REAL_VALUE_ISNAN (*rv) && !REAL_VALUE_NEGATIVE (*rv))
+    {
+      *imm_p = 9;
+      return true;
+    }
+
+  /* -0.0 is 16.  */
+  if (REAL_VALUE_MINUS_ZERO (*rv))
+    {
+      *imm_p = 16;
+      return true;
+    }
+
+  /* The other values are all integers 1..7, and -1..-7.  */
+  if (!real_isinteger (rv, mode))
+    return false;
+
+  HOST_WIDE_INT value = real_to_integer (rv);
+  if (value >= 1 && value <= 7)
+    {
+      *imm_p = value;
+      return true;
+    }
+  else if (value >= -7 && value <= -1)
+    {
+      /* Subtraction is used because value is negative.  */
+      *imm_p = 16 - value;
+      return true;
+    }
+
+  /* We can't load the value with LXVKQ.  */
+  return false;
+}
+
 const char *
 output_vec_const_move (rtx *operands)
 {
@@ -6738,6 +6822,7 @@ output_vec_const_move (rtx *operands)
       bool dest_vmx_p = ALTIVEC_REGNO_P (REGNO (dest));
       int xxspltib_value = 256;
       int num_insns = -1;
+      int lxvkq_immediate = 0;
 
       if (zero_constant (vec, mode))
 	{
@@ -6772,6 +6857,12 @@ output_vec_const_move (rtx *operands)
 
       if (xxsplti32dx_operand (vec, mode))
 	return "#";
+
+      if (lxvkq_constant_p (vec, mode, &lxvkq_immediate))
+	{
+	  operands[2] = GEN_INT (lxvkq_immediate);
+	  return "lxvkq %x0,%2";
+	}
 
       if (TARGET_P9_VECTOR
 	  && xxspltib_constant_p (vec, mode, &num_insns, &xxspltib_value))
@@ -13457,6 +13548,7 @@ rs6000_output_move_128bit (rtx operands[])
   int src_regno;
   bool dest_gpr_p, dest_fp_p, dest_vmx_p, dest_vsx_p;
   bool src_gpr_p, src_fp_p, src_vmx_p, src_vsx_p;
+  int lxvkq_immediate = 0;
 
   if (REG_P (dest))
     {
@@ -13601,6 +13693,14 @@ rs6000_output_move_128bit (rtx operands[])
     }
 
   /* Constants.  */
+  else if (dest_vmx_p
+	   && CONST_DOUBLE_P (src)
+	   && lxvkq_constant_p (src, mode, &lxvkq_immediate))
+    {
+      operands[2] = GEN_INT (lxvkq_immediate);
+      return "lxvkq %x0,%2";
+    }
+
   else if (dest_regno >= 0
 	   && (CONST_INT_P (src)
 	       || CONST_WIDE_INT_P (src)
@@ -24300,6 +24400,7 @@ static struct rs6000_opt_mask const rs6000_opt_masks[] =
   { "hard-dfp",			OPTION_MASK_DFP,		false, true  },
   { "htm",			OPTION_MASK_HTM,		false, true  },
   { "isel",			OPTION_MASK_ISEL,		false, true  },
+  { "lxvkq",			OPTION_MASK_LXVKQ,		false, true  },
   { "mfcrf",			OPTION_MASK_MFCRF,		false, true  },
   { "mfpgpr",			0,				false, true  },
   { "mma",			OPTION_MASK_MMA,		false, true  },
