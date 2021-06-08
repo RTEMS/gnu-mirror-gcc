84f081332969ec50956570eecc8e5be98f6238d7
Michael Meissner <meissner@linux.ibm.com>
2021-06-08 00:10:27 -0400

    Revert patch.
    gcc/
    2021-06-08  Michael Meissner  <meissner@linux.ibm.com>
    
    	Revert patch.
    	* config/rs6000/rs6000.c (rs6000_emit_minmax): Add support for ISA
    	3.1 IEEE 128-bit floating point xsmaxcqp and xsmincqp
    	instructions.
    	* config/rs6000/rs6000.md (s<minmax><mode>3, IEEE128 iterator):
    	New insns.
    
    gcc/testsuite/
    2021-06-08  Michael Meissner  <meissner@linux.ibm.com>
    
    	Revert patch.
    	* gcc.target/powerpc/float128-minmax-2.c: New test.
    	* gcc.target/powerpc/float128-minmax.c: Adjust expected code for
    	power10.
    	* lib/target-supports.exp (check_effective_target_has_arch_pwr10):
    	New target support.

--- gcc/testsuite/gcc.target/powerpc/float128-minmax.c
+++ gcc/testsuite/gcc.target/powerpc/float128-minmax.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target lp64 } } */
 /* { dg-require-effective-target powerpc_p9vector_ok } */
 /* { dg-require-effective-target float128 } */
-/* { dg-options "-O2 -ffast-math" } */
+/* { dg-options "-mpower9-vector -O2 -ffast-math" } */
 
 #ifndef TYPE
 #define TYPE _Float128
@@ -12,8 +12,5 @@
 TYPE f128_min (TYPE a, TYPE b) { return __builtin_fminf128 (a, b); }
 TYPE f128_max (TYPE a, TYPE b) { return __builtin_fmaxf128 (a, b); }
 
-/* Note power10 has native min/max instructions.  */
-/* { dg-final { scan-assembler     {\mxscmpuqp\M} } { target { ! has_arch_pwr10 } } } */
-/* { dg-final { scan-assembler     {\mxsmincqp\M} } { target {   has_arch_pwr10 } } } */
-/* { dg-final { scan-assembler     {\mxsmaxcqp\M} } { target {   has_arch_pwr10 } } } */
+/* { dg-final { scan-assembler     {\mxscmpuqp\M} } } */
 /* { dg-final { scan-assembler-not {\mbl\M}       } } */
