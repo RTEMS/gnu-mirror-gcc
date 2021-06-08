5075e1bf8216f9ce123f920d198c7759244c4dbb
Michael Meissner <meissner@linux.ibm.com>
2021-06-07 23:48:55 -0400

    Add IEEE 128-bit min/max support on PowerPC.
    This patch adds the support for the IEEE 128-bit floating point C minimum and
    maximum instructions.  The next patch will add the support for using the
    compare and set mask instruction to implement conditional moves.
    
    This patch does not try to re-use the code used for SF/DF min/max
    support.  It defines a separate insn for the IEEE 128-bit support.  It
    uses the code iterator <minmax> to simplify adding both operations.
    
    GCC will not convert ternary operations into using min/max instructions
    provided in this patch unless the user uses -Ofast or similar switches due to
    issues with NaNs.  The next patch that adds conditional move instructions will
    enable the ternary conversion in many cases.
    
    The float128-minmax.c test generates the xsmincqp and xsmaxcqp
    instructions on power10.  This patch adjusts the test to accomidate this.
    
    gcc/
    2021-06-07  Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/rs6000.c (rs6000_emit_minmax): Add support for ISA
    	3.1 IEEE 128-bit floating point xsmaxcqp and xsmincqp
    	instructions.
    	* config/rs6000/rs6000.md (s<minmax><mode>3, IEEE128 iterator):
    	New insns.
    
    gcc/testsuite/
    2021-06-07  Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/float128-minmax-2.c: New test.
    	* gcc.target/powerpc/float128-minmax.c: Adjust expected code for
    	power10.
    	* lib/target-supports.exp (check_effective_target_has_arch_pwr10):
    	New target support.

--- gcc/testsuite/gcc.target/powerpc/float128-minmax.c
+++ gcc/testsuite/gcc.target/powerpc/float128-minmax.c
@@ -1,7 +1,7 @@
 /* { dg-do compile { target lp64 } } */
 /* { dg-require-effective-target powerpc_p9vector_ok } */
 /* { dg-require-effective-target float128 } */
-/* { dg-options "-mpower9-vector -O2 -ffast-math" } */
+/* { dg-options "-O2 -ffast-math" } */
 
 #ifndef TYPE
 #define TYPE _Float128
@@ -12,5 +12,8 @@
 TYPE f128_min (TYPE a, TYPE b) { return __builtin_fminf128 (a, b); }
 TYPE f128_max (TYPE a, TYPE b) { return __builtin_fmaxf128 (a, b); }
 
-/* { dg-final { scan-assembler     {\mxscmpuqp\M} } } */
+/* Note power10 has native min/max instructions.  */
+/* { dg-final { scan-assembler     {\mxscmpuqp\M} } { target { ! has_arch_pwr10 } } } */
+/* { dg-final { scan-assembler     {\mxsmincqp\M} } { target {   has_arch_pwr10 } } } */
+/* { dg-final { scan-assembler     {\mxsmaxcqp\M} } { target {   has_arch_pwr10 } } } */
 /* { dg-final { scan-assembler-not {\mbl\M}       } } */
