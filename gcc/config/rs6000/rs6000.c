ccf32f3b871975f133cfd55072e71c9011d9c849
Michael Meissner <meissner@linux.ibm.com>
2021-09-06 20:13:52 -0400

    Generate XXSPLTIW on power10.
    This patch adds support to automatically generate the ISA 3.1 XXSPLTIW
    instruction for V8HImode, V4SImode, and V4SFmode vectors.  It does this by
    adding support for vector constants that can be used, and adding a
    VEC_DUPLICATE pattern to generate the actual XXSPLTIW instruction.
    
    I rewrote the XXSPLTW built-in functions to use VEC_DUPLICATE instead of
    UNSPEC.
    
    This patch also updates the insn counts in the vec-splati-runnable.c test to
    work with the new option to use XXSPLTIW to load up some vector constants.
    
    I added 4 new tests to test loading up V16QI, V8HI, V4SI, and V4SF vector
    constants.
    
    At the present time, XXSPLTIW geneation is disabled by default.
    
    2021-09-06  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/constraints.md (eW): New constraint.
    	* config/rs6000/predicates.md (xxspltiw_operand): New predicate.
    	(easy_vector_constant): If we can use XXSPLTIW, the vector
    	constant is easy.
    	* config/rs6000/rs6000-protos.h (xxspltiw_constant_p): New
    	declaration.
    	* config/rs6000/rs6000.c (xxspltib_constant_p): If we can generate
    	XXSPLTIW, don't generate a XXSPLTIB and an extend instruction.
    	(const_vector_all_elements_equal_p): New function.
    	(xxspltiw_constant_p): New function.
    	(output_vec_const_move): Add support for loading up vector
    	constants with XXSPLTIW.
    	(prefixed_permute_p): Recognize xxspltiw instructions as
    	prefixed.
    	* config/rs6000/rs6000.opt (-mxxspltiw): New debug switch.
    	* config/rs6000/vsx.md (vsx_mov<mode>_64bit): Add support for
    	constants loaded with XXSPLTIW.
    	(vsx_mov<mode>_32bit): Likewise.
    	(vsx_splat_v8hi_xxspltiw): New insn.
    	(vsx_splat_v4si_xxspltiw): New insn.
    	(vsx_splat_v4sf_xxspltiw): New insn.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/vec-splat-constant-v16qi.c: New test.
    	* gcc.target/powerpc/vec-splat-constant-v4sf.c: New test.
    	* gcc.target/powerpc/vec-splat-constant-v4si.c: New test.
    	* gcc.target/powerpc/vec-splat-constant-v8hi.c: New test.
    	* gcc.target/powerpc/vec-splati-runnable.c: Update insn counts.

--- gcc/config/rs6000/rs6000.c
+++ gcc/config/rs6000/rs6000.c
@@ -6583,11 +6583,173 @@ xxspltib_constant_p (rtx op,
   else if (IN_RANGE (value, -1, 0))
     *num_insns_ptr = 1;
 
+  /* See if we could generate XXSPLTIW directly.  */
+  else if (xxspltiw_operand (op, mode))
+    return false;
+
   else
     *num_insns_ptr = 2;
 
   *constant_ptr = (int) value;
   return true;
+}
+
+/* Return true if the argument is a constant vector where all elements are the
+   same.  */
+
+static bool
+const_vector_all_elements_equal_p (rtx op, machine_mode mode)
+{
+  if (!CONST_VECTOR_P (op))
+    return false;
+
+  rtx element = CONST_VECTOR_ELT (op, 0);
+  if (!CONST_INT_P (element) && !CONST_DOUBLE_P (element))
+    return false;
+
+  for (size_t i = 1; i < GET_MODE_NUNITS (mode); i++)
+    if (!rtx_equal_p (element, CONST_VECTOR_ELT (op, i)))
+      return false;
+
+  return true;
+}
+
+/* Return true if OP is of the given MODE and can be synthesized with ISA 3.1
+   XXSPLTIW instruction.
+
+   Return the constant via CONSTANT_PTR to use in the XXSPLTIW instruction.
+   The assembler does not like negative numbers for XXSPLTIW, so we need to
+   return a 16-bit unsigned value.  */
+
+bool
+xxspltiw_constant_p (rtx op,
+		     machine_mode mode,
+		     HOST_WIDE_INT *constant_ptr)
+{
+  HOST_WIDE_INT value;
+
+  *constant_ptr = 0;
+
+  if (!TARGET_PREFIXED || !TARGET_VSX || !TARGET_XXSPLTIW)
+    return false;
+
+  if (!CONST_VECTOR_P (op))
+    return true;
+
+  rtx element0 = CONST_VECTOR_ELT (op, 0);
+
+  switch (mode)
+    {
+      /* V4SImode constant vectors that have the same element are can be used
+	 with XXSPLTIW.  */
+    case V4SImode:
+      if (!const_vector_all_elements_equal_p (op, mode))
+	return false;
+
+      /* Don't return true if we can use the shorter vspltisw instruction.  */
+      value = INTVAL (element0);
+      if (EASY_VECTOR_15 (value))
+	return false;
+
+      *constant_ptr = value & 0xffffffff;
+      return true;
+
+      /* V4SFmode constant vectors that have the same element are
+	 can be used with XXSPLTIW.  */
+    case V4SFmode:
+      if (!const_vector_all_elements_equal_p (op, mode))
+	return false;
+
+      /* Don't return true for 0.0f, since that can be created with
+	 xxspltib or xxlxor.  */
+      if (element0 == CONST0_RTX (SFmode))
+	return false;
+
+      value = rs6000_const_f32_to_i32 (element0);
+      *constant_ptr = value & 0xffffffff;
+      return true;
+
+      /* V8Hmode constant vectors that have the same element are can be used
+	 with XXSPLTIW.  */
+    case V8HImode:
+      if (const_vector_all_elements_equal_p (op, mode))
+	{
+	  /* Don't return true if we can use the shorter vspltish instruction.  */
+	  value = INTVAL (element0);
+	  if (EASY_VECTOR_15 (value))
+	    return false;
+
+	  value &= 0xffff;
+	  *constant_ptr = (value << 16) | value;
+	  return true;
+	}
+
+      else
+	{
+	  /* Check if all even elements are the same and all odd elements are
+	     the same.  */
+	  rtx element1 = CONST_VECTOR_ELT (op, 1);
+
+	  if (!CONST_INT_P (element1))
+	    return false;
+
+	  for (size_t i = 2; i < GET_MODE_NUNITS (V8HImode); i += 2)
+	    if (!rtx_equal_p (element0, CONST_VECTOR_ELT (op, i))
+		|| !rtx_equal_p (element1, CONST_VECTOR_ELT (op, i + 1)))
+	      return false;
+
+	  HOST_WIDE_INT value0 = INTVAL (element0) & 0xffff;
+	  HOST_WIDE_INT value1 = INTVAL (element1) & 0xffff;
+
+	  if (!BYTES_BIG_ENDIAN)
+	    std::swap (value0, value1);
+
+	  *constant_ptr = (value0 << 16) | value1;
+	  return true;
+	}
+
+      /* V16QI constant vectors that have the first four elements identical to
+	 the next set of 4 elements, and so forth can generate XXSPLTIW.  */
+    case V16QImode:
+	{
+	  if (xxspltib_constant_nosplit (op, mode))
+	    return false;
+
+	  rtx element1 = CONST_VECTOR_ELT (op, 1);
+	  rtx element2 = CONST_VECTOR_ELT (op, 2);
+	  rtx element3 = CONST_VECTOR_ELT (op, 3);
+
+	  if (!CONST_INT_P (element0) || !CONST_INT_P (element1)
+	      || !CONST_INT_P (element2) || !CONST_INT_P (element3))
+	    return false;
+
+	  for (size_t i = 4; i < GET_MODE_NUNITS (V16QImode); i += 4)
+	    if (!rtx_equal_p (element0, CONST_VECTOR_ELT (op, i))
+		|| !rtx_equal_p (element1, CONST_VECTOR_ELT (op, i + 1))
+		|| !rtx_equal_p (element2, CONST_VECTOR_ELT (op, i + 2))
+		|| !rtx_equal_p (element3, CONST_VECTOR_ELT (op, i + 3)))
+	      return false;
+
+	  HOST_WIDE_INT value0 = INTVAL (element0) & 0xff;
+	  HOST_WIDE_INT value1 = INTVAL (element1) & 0xff;
+	  HOST_WIDE_INT value2 = INTVAL (element2) & 0xff;
+	  HOST_WIDE_INT value3 = INTVAL (element3) & 0xff;
+
+	  if (BYTES_BIG_ENDIAN)
+	    *constant_ptr = ((value0 << 24) | (value1 << 16) | (value2 << 8)
+			     | value3);
+	  else
+	    *constant_ptr = ((value3 << 24) | (value2 << 16) | (value1 << 8)
+			     | value0);
+
+	  return true;
+	}
+
+    default:
+      break;
+    }
+
+  return false;
 }
 
 /* Return true if OP is of the given MODE and can be synthesized with ISA 3.1
@@ -6779,6 +6941,7 @@ output_vec_const_move (rtx *operands)
     {
       bool dest_vmx_p = ALTIVEC_REGNO_P (REGNO (dest));
       int xxspltib_value = 256;
+      HOST_WIDE_INT xxspltiw_value = 0;
       HOST_WIDE_INT xxspltidp_value = 0;
       int num_insns = -1;
       int lxvkq_immediate = 0;
@@ -6808,6 +6971,12 @@ output_vec_const_move (rtx *operands)
 
 	  else
 	    gcc_unreachable ();
+	}
+
+      if (xxspltiw_constant_p (vec, mode, &xxspltiw_value))
+	{
+	  operands[2] = GEN_INT (xxspltiw_value);
+	  return "xxspltiw %x0,%2";
 	}
 
       if (xxspltidp_constant_p (vec, mode, &xxspltidp_value))
@@ -26593,6 +26762,11 @@ prefixed_permute_p (rtx_insn *insn)
 
   switch (mode)
     {
+    case V8HImode:
+    case V4SImode:
+    case V4SFmode:
+      return xxspltiw_operand (src, mode);
+
     case DFmode:
     case SFmode:
     case V2DFmode:
