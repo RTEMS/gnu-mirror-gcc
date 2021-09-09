a32255191fd82b2d7e3c3a49ed2c1742ba6d726d
Martin Liska <mliska@suse.cz>
2021-09-09 13:02:24 +0200

    gcov: make profile merging smarter

--- gcc/gcov.c
+++ gcc/gcov.c
@@ -1814,6 +1814,8 @@ read_graph_file (void)
 	       bbg_file_name, v, e);
     }
   bbg_stamp = gcov_read_unsigned ();
+  /* Read checksum.  */
+  gcov_read_unsigned ();
   bbg_cwd = xstrdup (gcov_read_string ());
   bbg_supports_has_unexecuted_blocks = gcov_read_unsigned ();
 
--- libgcc/libgcov-util.c
+++ libgcc/libgcov-util.c
@@ -307,8 +307,8 @@ read_gcda_file (const char *filename)
     obj_info->filename = str_dup;
   }
 
-  /* Read stamp.  */
-  obj_info->stamp = gcov_read_unsigned ();
+  /* Read checksum.  */
+  obj_info->checksum = gcov_read_unsigned ();
 
   while (1)
     {
