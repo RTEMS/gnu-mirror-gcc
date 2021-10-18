86ae6e0d17cb144be310c96dd425383000534e45
Michael Meissner <meissner@linux.ibm.com>
2021-10-18 15:09:45 -0400

    Add LXVKQ support.
    This patch adds support to generate the LXVKQ instruction to load specific
    IEEE-128 floating point constants.
    
    Compared to the last time I submitted this patch, I modified it so that it
    uses the bit pattern of the vector to see if it can generate the LXVKQ
    instruction.  This means on a little endian Power<xxx> system, the
    following code will generate a LXVKQ 34,16 instruction:
    
        vector long long foo (void)
        {
          return (vector long long) { 0x0000000000000000, 0x8000000000000000 };
        }
    
    because that vector pattern is the same bit pattern as -0.0F128.
    
    2021-10-18  Michael Meissner  <meissner@the-meissners.org>
    
    gcc/
    
    	* config/rs6000/constraints.md (eQ): New constraint.
    	* config/rs6000/predicates.md (easy_fp_constant): Add support for
    	generating the LXVKQ instruction.
    	(easy_vector_constant_ieee128): New predicate.
    	(easy_vector_constant): Add support for generating the LXVKQ
    	instruction.
    	* config/rs6000/rs6000-protos.h (rs6000_vec_concat): Add fields
    	for generating LXVKQ.
    	* config/rs6000/rs6000.c (output_vec_const_move): Add support for
    	generating LXVKQ.
    	(vec_const_use_lxvkq): New function.
    	* config/rs6000/rs6000.opt (-mlxvkq): New debug option.
    	* config/rs6000/vsx.md (vsx_mov<mode>_64bit): Add support for
    	generating LXVKQ.
    	(vsx_mov<mode>_32bit): Likewise.
    	* doc/md.texi (PowerPC and IBM RS6000 constraints): Document the
    	eQ constraint.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/float128-constant.c: New test.

--- gcc/config/rs6000/constraints.md
+++ gcc/config/rs6000/constraints.md
@@ -219,6 +219,11 @@
   "A constant that can be loaded into a VSX register with one prefixed insn."
   (match_operand 0 "vsx_prefixed_constant"))
 
+;; 128-bit IEEE 128-bit constant
+(define_constraint "eQ"
+  "An IEEE 128-bit constant that can be loaded with the LXVKQ instruction."
+  (match_operand 0 "easy_vector_constant_ieee128"))
+
 ;; Floating-point constraints.  These two are defined so that insn
 ;; length attributes can be calculated exactly.
 
--- gcc/config/rs6000/rs6000-protos.h
+++ gcc/config/rs6000/rs6000-protos.h
@@ -240,6 +240,7 @@ typedef struct {
   unsigned char bytes[VECTOR_CONST_BYTES];
 
   unsigned int xxspltidp_immediate;	/* Immediate value for XXSPLTIDP.  */
+  unsigned int lxvkq_immediate;		/* Immediate value for LXVKQ.  */
   bool fp_constant_p;			/* Is the constant floating point?  */
   bool all_double_words_same;		/* Are the double words all equal?  */
   bool all_words_same;			/* Are the words all equal?  */
@@ -250,6 +251,7 @@ typedef struct {
 extern bool vec_const_to_bytes (rtx, machine_mode, rs6000_vec_const *);
 extern bool vec_const_use_xxspltidp (rs6000_vec_const *);
 extern bool vec_const_use_xxspltiw (rs6000_vec_const *);
+extern bool vec_const_use_lxvkq (rs6000_vec_const *);
 #endif /* RTX_CODE */
 
 #ifdef TREE_CODE
