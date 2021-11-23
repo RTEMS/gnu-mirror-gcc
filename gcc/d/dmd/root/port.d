D-Programming-GDC/gcc

d: Import dmd v2.098.0-beta.1 (b8384668f)
gcc/d/ChangeLog:

	* dmd/MERGE: Merge upstream dmd b8384668f.
	* dmd/README.md: New file.
	* dmd/VERSION: New file.
	* dmd/access.d: New file.
	* dmd/aggregate.d: New file.
	* dmd/aliasthis.d: New file.
	* dmd/apply.d: New file.
	* dmd/arrayop.d: New file.
	* dmd/arraytypes.d: New file.
	* dmd/ast_node.d: New file.
	* dmd/astcodegen.d: New file.
	* dmd/astenums.d: New file.
	* dmd/attrib.d: New file.
	* dmd/blockexit.d: New file.
	* dmd/builtin.d: New file.
	* dmd/canthrow.d: New file.
	* dmd/chkformat.d: New file.
	* dmd/clone.d: New file.
	* dmd/compiler.d: New file.
	* dmd/complex.d: New file.
	* dmd/cond.d: New file.
	* dmd/constfold.d: New file.
	* dmd/cparse.d: New file.
	* dmd/cppmangle.d: New file.
	* dmd/ctfeexpr.d: New file.
	* dmd/ctorflow.d: New file.
	* dmd/dcast.d: New file.
	* dmd/dclass.d: New file.
	* dmd/declaration.d: New file.
	* dmd/delegatize.d: New file.
	* dmd/denum.d: New file.
	* dmd/dimport.d: New file.
	* dmd/dinterpret.d: New file.
	* dmd/dmacro.d: New file.
	* dmd/dmangle.d: New file.
	* dmd/dmodule.d: New file.
	* dmd/doc.d: New file.
	* dmd/dscope.d: New file.
	* dmd/dstruct.d: New file.
	* dmd/dsymbol.d: New file.
	* dmd/dsymbolsem.d: New file.
	* dmd/dtemplate.d: New file.
	* dmd/dtoh.d: New file.
	* dmd/dversion.d: New file.
	* dmd/entity.d: New file.
	* dmd/errors.d: New file.
	* dmd/escape.d: New file.
	* dmd/expression.d: New file.
	* dmd/expressionsem.d: New file.
	* dmd/foreachvar.d: New file.
	* dmd/func.d: New file.
	* dmd/globals.d: New file.
	* dmd/gluelayer.d: New file.
	* dmd/hdrgen.d: New file.
	* dmd/iasm.d: New file.
	* dmd/iasmgcc.d: New file.
	* dmd/id.d: New file.
	* dmd/identifier.d: New file.
	* dmd/impcnvtab.d: New file.
	* dmd/imphint.d: New file.
	* dmd/init.d: New file.
	* dmd/initsem.d: New file.
	* dmd/inline.d: New file.
	* dmd/intrange.d: New file.
	* dmd/json.d: New file.
	* dmd/lambdacomp.d: New file.
	* dmd/lexer.d: New file.
	* dmd/mtype.d: New file.
	* dmd/no

gcc/d/dmd/root/port.d
@@ -1,0 +1,47 @@
+/**
+ * Portable routines for functions that have different implementations on different platforms.
+ *
+ * Copyright: Copyright (C) 1999-2021 by The D Language Foundation, All Rights Reserved
+ * Authors:   Walter Bright, http://www.digitalmars.com
+ * License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Source:    $(LINK2 https://github.com/dlang/dmd/blob/master/src/dmd/root/port.d, root/_port.d)
+ * Documentation:  https://dlang.org/phobos/dmd_root_port.html
+ * Coverage:    https://codecov.io/gh/dlang/dmd/src/master/src/dmd/root/port.d
+ */
+
+module dmd.root.port;
+
+nothrow @nogc:
+
+extern (C++) struct Port
+{
+    nothrow @nogc:
+
+    static int memicmp(scope const char* s1, scope const char* s2, size_t n) pure;
+
+    static char* strupr(char* s) pure;
+
+    static bool isFloat32LiteralOutOfRange(scope const(char)* s);
+
+    static bool isFloat64LiteralOutOfRange(scope const(char)* s);
+
+    // Little endian
+    static void writelongLE(uint value, scope void* buffer) pure;
+
+    // Little endian
+    static uint readlongLE(scope const void* buffer) pure;
+
+    // Big endian
+    static void writelongBE(uint value, scope void* buffer) pure;
+
+    // Big endian
+    static uint readlongBE(scope const void* buffer) pure;
+
+    // Little endian
+    static uint readwordLE(scope const void* buffer) pure;
+
+    // Big endian
+    static uint readwordBE(scope const void* buffer) pure;
+
+    static void valcpy(scope void *dst, ulong val, size_t size) pure;
+}

RTEMS/gnu-mirror-gcc

gcc/d/dmd/root/port.d
@@ -1,0 +1,49 @@
+/**
+ * Portable routines for functions that have different implementations on different platforms.
+ *
+ * Copyright: Copyright (C) 1999-2021 by The D Language Foundation, All Rights Reserved
+ * Authors:   Walter Bright, http://www.digitalmars.com
+ * License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Source:    $(LINK2 https://github.com/dlang/dmd/blob/master/src/dmd/root/port.d, root/_port.d)
+ * Documentation:  https://dlang.org/phobos/dmd_root_port.html
+ * Coverage:    https://codecov.io/gh/dlang/dmd/src/master/src/dmd/root/port.d
+ */
+
+module dmd.root.port;
+
+import core.stdc.stdint;
+
+nothrow @nogc:
+
+extern (C++) struct Port
+{
+    nothrow @nogc:
+
+    static int memicmp(scope const char* s1, scope const char* s2, size_t n) pure;
+
+    static char* strupr(char* s) pure;
+
+    static bool isFloat32LiteralOutOfRange(scope const(char)* s);
+
+    static bool isFloat64LiteralOutOfRange(scope const(char)* s);
+
+    // Little endian
+    static void writelongLE(uint value, scope void* buffer) pure;
+
+    // Little endian
+    static uint readlongLE(scope const void* buffer) pure;
+
+    // Big endian
+    static void writelongBE(uint value, scope void* buffer) pure;
+
+    // Big endian
+    static uint readlongBE(scope const void* buffer) pure;
+
+    // Little endian
+    static uint readwordLE(scope const void* buffer) pure;
+
+    // Big endian
+    static uint readwordBE(scope const void* buffer) pure;
+
+    static void valcpy(scope void *dst, uint64_t val, size_t size) pure;
+}