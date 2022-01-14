bce0fe17ccfddf43b49f737a88de3796a068fc0e
Martin Liska <mliska@suse.cz>
2022-01-14 15:37:40 +0100

    asan, testsuite: remove 2 unused files
    and rename foo.cc files to foo-aux.cc.

--- gcc/testsuite/g++.dg/asan/asan_test.C
+++ gcc/testsuite/g++.dg/asan/asan_test.C
@@ -1,7 +1,7 @@
 // { dg-do run { target { { *-*-linux* i?86-*-freebsd* x86_64-*-freebsd* } && { { ! { i?86-*-linux* x86_64-*-linux* i?86-*-freebsd* x86_64-*-freebsd* } } || sse2_runtime } } } }
 // { dg-skip-if "" { *-*-* } { "*" } { "-O2" } }
 // { dg-skip-if "" { *-*-* } { "-flto" } { "" } }
-// { dg-additional-sources "asan_globals_test-wrapper.cc" }
+// { dg-additional-sources "asan_globals_test-wrapper-aux.cc" }
 // { dg-options "-std=c++11 -fsanitize=address -fno-builtin -Wall -Werror -Wno-alloc-size-larger-than -Wno-array-bounds -Wno-stringop-overflow -Wno-stringop-overread -g -DASAN_UAR=0 -DASAN_HAS_EXCEPTIONS=1 -DASAN_HAS_BLACKLIST=0 -DSANITIZER_USE_DEJAGNU_GTEST=1 -lasan -lpthread" }
 // { dg-additional-options "-ldl" { target { ! *-*-freebsd* } } }
 // { dg-additional-options "-DASAN_NEEDS_SEGV=1" { target { ! arm*-*-* } } }
@@ -12,7 +12,7 @@
 // { dg-set-target-env-var ASAN_OPTIONS "handle_segv=2" }
 // { dg-final { asan-gtest } }
 
-#include "asan_test.cc"
-#include "asan_mem_test.cc"
-#include "asan_str_test.cc"
-#include "asan_oob_test.cc"
+#include "asan_test-aux.cc"
+#include "asan_mem_test-aux.cc"
+#include "asan_str_test-aux.cc"
+#include "asan_oob_test-aux.cc"
