9929e936ce4b2c2de057f560c0dcb9bbca339c72
Michael Meissner <meissner@linux.ibm.com>
2022-03-18 13:38:21 -0400

    Improve extendditi2 on PowerPC.
    This patch allows using extenditi2 on power9 systems as well as power10
    systems.  On power8 and previous systems, the generic machine indepenent
    code will be used.  This patch will be a building block to support in a
    future patch PR target/103109 which wants to optimize 128-bit some integer
    multiply-add combinations to use the power9 maddld, maddhd, maddhdu
    instructions.
    
    This patch improves code generation when extending a GPR to a vector
    register by generating the sign extend part in a GPR register before
    moving the value over to the vector register with the mtvsrdd
    instruction.
    
    2022-03-18   Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    	* config/rs6000/vsx.md (extendditi2): Add power9 support.  Improve
    	code generation when doing direct moves.
    
    gcc/testsuite/
    	* gcc.target/powerpc/pr104698-2.c: Update insn counts.

--- gcc/testsuite/gcc.target/powerpc/pr104698-2.c
+++ gcc/testsuite/gcc.target/powerpc/pr104698-2.c
@@ -3,31 +3,43 @@
 /* { dg-options "-mdejagnu-cpu=power10 -O2" } */
 
 /* PR target/104694 involved GCC generating vextsd2q to convent long long to
-   __int128_t when the long long value was in the GPR register.  This test
-   verifies that if the result is in the Altivec registers, we still want to
-   generate vextsd2q.  We use __int128_t to indicate that we want the result of
-   the conversion to be in an Altivec register. */
+   __int128_t when the long long value was in the GPR register.
+
+   This test verifies that the code we want when converting a GPR to a vector
+   register, that we generate the sign extend in the GPR register, and then do
+   a mtvsrdd to get it into the vector register.  */
 
 void do_div_1 (__int128_t *p, __int128_t *q, long long r)
 {
-  *p = *q / r;		/* mtvsrdd, vextsd2q, vdivsq.  */
+  *p = *q / r;		/* sradi, mtvsrdd, vdivsq.  */
 }
 
-/* Test the optimization in vsx.md to use lxvrdx instead of ld and mtvsrdd if
-   the value is coming from memory.  */
+/* Test the optimization in vsx.md to use lxvrdx instead of ld and then do
+   vextsd2q to sign extend it.  */
 
 void do_div_2 (__int128_t *p, __int128_t *q, long long *r)
 {
   *p = *q / r[2];	/* lxvrdx, vextsd2q, vdivsq.  */
 }
 
+/* If the 64-bit integer is in a vector register (after conversion from
+   double), don't move it back to a GPR register to do the sign extension.
+   Instead use vextsd2q.  */
+
+void do_div_3 (__int128_t *p, __int128_t *q, double r)
+{
+  *p = *q / (long long)r;	/* xscvdpsxds, xxpermdi, vextsd2q, vdivsq.  */
+}
+
+
 /* { dg-final { scan-assembler-not   {\mld\M}         } } */
 /* { dg-final { scan-assembler-not   {\mmfvsrd\M}     } } */
 /* { dg-final { scan-assembler-not   {\mmfvsrld\M}    } } */
-/* { dg-final { scan-assembler-not   {\msradi\M}      } } */
-/* { dg-final { scan-assembler-times {\mlxv\M}      2 } } */
+/* { dg-final { scan-assembler-times {\msradi\M}    1 } } */
+/* { dg-final { scan-assembler-times {\mlxv\M}      3 } } */
 /* { dg-final { scan-assembler-times {\mlxvrdx\M}   1 } } */
 /* { dg-final { scan-assembler-times {\mmtvsrdd\M}  1 } } */
-/* { dg-final { scan-assembler-times {\mstxv\M}     2 } } */
-/* { dg-final { scan-assembler-times {\mvdivsq\M}   2 } } */
+/* { dg-final { scan-assembler-times {\mstxv\M}     3 } } */
+/* { dg-final { scan-assembler-times {\mvdivsq\M}   3 } } */
 /* { dg-final { scan-assembler-times {\mvextsd2q\M} 2 } } */
+/* { dg-final { scan-assembler-times {\mxxpermdi\M} 1 } } */
