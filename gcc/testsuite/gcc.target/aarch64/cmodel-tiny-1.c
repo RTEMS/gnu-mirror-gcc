RTEMS/gnu-mirror-gcc

commit c675754a1788ddd741fb310d8d7ee612b95264f5

aarch64: Tweak validity of label/symbol_ref addresses
arch64_classify_address had code to allow:

  (mem (label_ref X))    [case (1)]

for any access that is word-sized or greater.  It also had code
to allow:

  (mem (symbol_ref X))   [case (2)]

when -mpc-relative-literal-loads is in effect and X is the address
of a function literal pool entry.  The assumption in both cases is
that X is within the range of a PC-relative LDR instruction.

The decision about whether:

  (set (reg R) (label/symbol_ref X))

is valid is instead made by aarch64_classify_symbol, with
SYMBOL_TINY_ABSOLUTE (despite the “ABSOLUTE”) meaning that
X is within the range of a PC-relative ADR instruction.

The two decisions were inconsistent in that, for the normal
“small” code model, aarch64_classify_symbol classified both a
normal label_ref and a normal symbol_ref as SYMBOL_SMALL_ABSOLUTE
rather than SYMBOL_TINY_ABSOLUTE.  Moves therefore required a
hi/lo split in cases where MEMs allowed a direct reference.

This then raises the question of which version is correct.
I think the move/aarch64_classify_symbol choice is right for
label_refs and the mem/aarch64_classify_address choice is right
for symbol_refs.

This difference probably doesn't matter much in practice, since
(a) we should rarely (if ever) directly load from a label_ref and
(b) we should rarely (if ever) need to take the address of a constant
pool entry.  But it helps later patches if we “fix” the inconsistency
and then make arch64_classify_address use aarch64_classify_symbol
rather than doing its own thing.

Although the patch is/was supposed to have little effect on code
generation, disallowing (1) has the effect of making label_refs
no longer satisfy memory_address_p when they need to be split into
hi/lo accesses.  This means that they are no longer satisfy
CONSTANT_ADDRESS_P.

The new behaviour makes label_refs consistent with symbol_refs:
they satisfy CONSTANT_ADDRESS_P if they are within the range of

gcc/config/aarch64/aarch64.c
@@ -10059,16 +10059,9 @@ aarch64_classify_address (struct aarch64_address_info *info,
 
       if (ldp_stp_mode == VOIDmode
 	  && GET_MODE_SIZE (mode).is_constant (&const_size)
-	  && const_size >= 4)
-	{
-	  rtx sym, addend;
-
-	  split_const (x, &sym, &addend);
-	  return (GET_CODE (sym) == LABEL_REF
-		   || (GET_CODE (sym) == SYMBOL_REF
-		       && aarch64_pcrelative_literal_loads
-		       && aarch64_function_literal_pool_address_p (x)));
-	}
+	  && const_size >= 4
+	  && aarch64_classify_symbolic_expression (x) == SYMBOL_TINY_ABSOLUTE)
+	return true;
       return false;
 
     case LO_SUM:
@@ -10910,12 +10903,14 @@ aarch64_print_operand (FILE *f, rtx x, int code)
 	  break;
 
 	case SYMBOL_REF:
+	case LABEL_REF:
 	  output_addr_const (f, x);
 	  break;
 
 	case CONST:
 	  if (any_plus_p (XEXP (x, 0))
-	      && GET_CODE (XEXP (XEXP (x, 0), 0)) == SYMBOL_REF)
+	      && (GET_CODE (XEXP (XEXP (x, 0), 0)) == SYMBOL_REF
+		  || GET_CODE (XEXP (XEXP (x, 0), 0)) == LABEL_REF))
 	    {
 	      output_addr_const (f, x);
 	      break;
@@ -17115,6 +17110,13 @@ aarch64_classify_symbol (rtx x, HOST_WIDE_INT offset)
       if (aarch64_sym_indirectly_accessed_p (x))
 	return aarch64_classify_capability_symbol (x, offset);
 
+      /* -mpc-relative-literal-loads tells us to assume that all (function)
+	 constant pool entries will be within the range of PC-relative LDR,
+	 which means that they must also be in range of ADR.  */
+      if (aarch64_pcrelative_literal_loads
+	  && aarch64_function_literal_pool_address_p (x))
+	return SYMBOL_TINY_ABSOLUTE;
+
       switch (aarch64_cmodel)
 	{
 	case AARCH64_CMODEL_TINY:
@@ -17165,8 +17167,7 @@ aarch64_classify_symbol (rtx x, HOST_WIDE_INT offset)
 	  /* This is alright even in PIC code as the constant
 	     pool reference is always PC relative and within
 	     the same translation unit.  */
-	  if (!aarch64_pcrelative_literal_loads
-	      && aarch64_function_literal_pool_address_p (x))
+	  if (aarch64_function_literal_pool_address_p (x))
 	    return SYMBOL_SMALL_ABSOLUTE;
 	  else
 	    return SYMBOL_FORCE_TO_MEM;

commit 021d8d117cbe7e5f1dbc4d3ac5ff28ddef25b740

aarch64: Avoid generating PC-relative stores
c675754a1788ddd741fb310d8d7ee612b95264f5 regressed -mcmodel=tiny
by allowing PC-relative stores such as “str x0, foo”, whereas
only PC-relative loads are supported.  Previously
aarch64_classify_address had:

      if (ldp_stp_mode == VOIDmode
	  && GET_MODE_SIZE (mode).is_constant (&const_size)
	  && const_size >= 4)
	{
	  rtx sym, addend;

	  split_const (x, &sym, &addend);
	  return (GET_CODE (sym) == LABEL_REF
		   || (GET_CODE (sym) == SYMBOL_REF
		       && aarch64_pcrelative_literal_loads
		       && aarch64_function_literal_pool_address_p (x)));
	}

and so allowed references to labels (which should never be needed here)
and references to function literal pools.  The patch instead made the
code test whether x is SYMBOL_TINY_ABSOLUTE, which is needed for
-mcmodel=tiny references to .capinit entries in .data.rel.ro.

However, as the fallout shows, we still need the test for a constant
pool entry.  It just doesn't need to be in the function literal pool.

As the comment in the patch says, this is really a hack.  The
pre-Morello handling of -mcmodel=tiny means that we generate:

	adr	x0, foo
	ldr	x0, [x0]

even though:

	ldr	x0, foo

would be OK too.  But fixing things so that we generate that
without:

	str	x0, foo

would be too much of a distraction.  It's also something that
should happen on trunk first.

gcc/config/aarch64/aarch64.c
@@ -10064,7 +10064,18 @@ aarch64_classify_address (struct aarch64_address_info *info,
 	  && GET_MODE_SIZE (mode).is_constant (&const_size)
 	  && const_size >= 4
 	  && aarch64_classify_symbolic_expression (x) == SYMBOL_TINY_ABSOLUTE)
-	return true;
+	{
+	  /* ??? This is a bit of a hack.  All SYMBOL_TINY_ABSOLUTE loads
+	     should be in range, not just function constant pool entries.
+	     However, to handle that properly, we would need to distinguish
+	     between load and store contexts.  This would probably mean
+	     defining TARGET_MEM_CONSTRAINT to something other than "m",
+	     keeping the current meaning of "m", and having a more relaxed
+	     constraint than "m" for loads.  */
+	  rtx base = strip_offset (x, &offset);
+	  if (SYMBOL_REF_P (base) && CONSTANT_POOL_ADDRESS_P (base))
+	    return true;
+	}
       return false;
 
     case LO_SUM:

gcc/testsuite/gcc.target/aarch64/cmodel-tiny-1.c
@@ -1,0 +1,5 @@
+/* { dg-do assemble } */
+/* { dg-options "-O2 -mcmodel=tiny" } */
+
+int x[5];
+void f() { x[3] = 1; }