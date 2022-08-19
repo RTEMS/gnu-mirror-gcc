5ee5c817f3f9e91a2769cbea0fbd8b8866142e13
Michael Meissner <meissner@linux.ibm.com>
2022-08-19 00:05:07 -0400

    Make __float128 use the _Float128 type.
    Currently GCC uses the long double type node for __float128 if long double is
    IEEE 128-bit.  It did not use the node for _Float128.
    
    Problems showed up if you call the nansq function to make a signaling NaN (nansq
    is mapped to nansf128).  Because the type node for _Float128 is different from
    __float128, the machine independent code converts signaling NaNs to quiet NaNs
    if the types are  not compatible.  The following tests used to fail when run on
    a system where long double is IEEE 128-bit:
    
    	gcc.dg/torture/float128-nan.c
    	gcc.target/powerpc/nan128-1.c
    
    This patch makes both __float128 and _Float128 use the same type node.
    
    This patch also looks to see if the caller function was passed TFmode.  If it
    was passed TFmode and the built-in function uses KFmode, switch the built-in
    function to the equivalent TFmode built-in.  Similarly, if it was passed TFmode
    and the built-in functions uses IFmode, switch the built-in function to the
    equivalent TFmode built-in.
    
    In the past the code would do the conversion based on what 128-bit floating
    point format was used.  This patch changes this to look at the 	arguments.  If
    any of the arguments use TFmode instead of KFmode or IFmode, it will do the
    conversion.
    
    Just in case the mode of the argument is different when we expand the built-in
    function, do a convert_move instead of copy_to_mode_reg, which will abort
    because the modes are different.
    
    2022-08-17   Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/rs6000-builtin.cc (rs6000_init_builtins): Always use the
    	_Float128 type for __float128.  Don't use the long double type.
    	(rs6000_expand_builtin): Look at the argument types to see if a TFmode
    	argument is passed to convert a KFmode or IFmode builtin to TFmode,
    	instead of just looking at the options.
    	* config/rs6000/rs6000.cc (rs6000_libgcc_floating_mode_supported_p):
    	_Float128/__float128 types always use KFmode now.
    	(rs6000_floatn_mode): Likewise.

--- gcc/config/rs6000/rs6000.cc
+++ gcc/config/rs6000/rs6000.cc
@@ -23724,13 +23724,8 @@ rs6000_libgcc_floating_mode_supported_p (scalar_float_mode mode)
     case E_TFmode:
       return true;
 
-      /* We only return true for KFmode if IEEE 128-bit types are supported, and
-	 if long double does not use the IEEE 128-bit format.  If long double
-	 uses the IEEE 128-bit format, it will use TFmode and not KFmode.
-	 Because the code will not use KFmode in that case, there will be aborts
-	 because it can't find KFmode in the Floatn types.  */
     case E_KFmode:
-      return TARGET_FLOAT128_TYPE && !TARGET_IEEEQUAD;
+      return TARGET_FLOAT128_TYPE;
 
     default:
       return false;
@@ -23764,7 +23759,7 @@ rs6000_floatn_mode (int n, bool extended)
 
 	case 64:
 	  if (TARGET_FLOAT128_TYPE)
-	    return (FLOAT128_IEEE_P (TFmode)) ? TFmode : KFmode;
+	    return KFmode;
 	  else
 	    return opt_scalar_float_mode ();
 
@@ -23788,7 +23783,7 @@ rs6000_floatn_mode (int n, bool extended)
 
 	case 128:
 	  if (TARGET_FLOAT128_TYPE)
-	    return (FLOAT128_IEEE_P (TFmode)) ? TFmode : KFmode;
+	    return KFmode;
 	  else
 	    return opt_scalar_float_mode ();
 
