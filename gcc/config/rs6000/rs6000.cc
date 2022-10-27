e431ac889b606a9c78f628d5695022f2ccc1903b
Michael Meissner <meissner@linux.ibm.com>
2022-10-27 01:07:44 -0400

    Add LAGEN and PLAGEN support.
    2022-10-27   Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/rs6000-cpus.def (POWERPC_MASKS): Add -mlagen.
    	* config/rs6000/rs6000.cc (rs6000_file_start): Add macros for fake lagen
    	and plagen instructions.
    	(rs6000_opt_masks): Add -mlagen.
    	* config/rs6000/rs6000.md (lagendi3): New insn for -mlagen.
    	(plagendi3_nora): Likewise.
    	(plagendi3): Likewise.
    	(plagendi3_noshift): Likewise.
    	* config/rs6000/rs6000.opt (-mlagen): New option.

--- gcc/config/rs6000/rs6000.cc
+++ gcc/config/rs6000/rs6000.cc
@@ -6089,6 +6089,20 @@ rs6000_file_start (void)
 
   if (DEFAULT_ABI == ABI_ELFv2)
     fprintf (file, "\t.abiversion 2\n");
+
+  if (TARGET_LAGEN)
+    {
+      fprintf (file, "\n");
+      fprintf (file, "\t.macro lagen rt,ra,rb,n\n");
+      fprintf (file, "\taddi \\rt,\\rt,0\n");
+      fprintf (file, "\t.endm\n");
+      fprintf (file, "\n");
+
+      fprintf (file, "\t.macro plagen rt,d,rb,n\n");
+      fprintf (file, "\tpaddi \\rt,\\rt,0\n");
+      fprintf (file, "\t.endm\n");
+      fprintf (file, "\n");
+    }
 }
 
 
@@ -24324,6 +24338,7 @@ static struct rs6000_opt_mask const rs6000_opt_masks[] =
   { "hard-dfp",			OPTION_MASK_DFP,		false, true  },
   { "htm",			OPTION_MASK_HTM,		false, true  },
   { "isel",			OPTION_MASK_ISEL,		false, true  },
+  { "lagen",			OPTION_MASK_LAGEN,		false, true  },
   { "mfcrf",			OPTION_MASK_MFCRF,		false, true  },
   { "mfpgpr",			0,				false, true  },
   { "mma",			OPTION_MASK_MMA,		false, true  },
