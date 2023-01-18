c69c092e3eed38677a7f26150097d86670ce0c42
Michael Meissner <meissner@linux.ibm.com>
2023-01-18 01:10:55 -0500

    PR target/107299: Update IEEE 128-bit types in PowerPC libgcc.
    This patch updates the IEEE 128-bit types in libgcc.
    
    At the moment, we cannot build GCC when the target uses IEEE 128-bit long
    doubles, such as building the compiler for a native Fedora 36 system.  The
    build dies when it is trying to build the _mulkc3.c and _divkc3 modules.
    
    This patch changes the IEEE 128-bit type (TFtype) in the IEEE 128-bit support
    to use either _Float128 or long double.  Previously we would use the __float128
    type.  In addition, the TCtype will use the same type along with _Complex.
    
    While it is desirable to ultimately have __float128 and _Float128 use the same
    internal type and mode within GCC, at present if you use the option
    -mabi=ieeelongdouble, the __float128 type will use the long double type.  We
    get an internal compiler error if we combine the signbitf128 built-in with a
    long double type.
    
    I've gone through several iterations of trying to fix this within GCC, and
    there are various problems that have come up.  I developed this alternative
    patch that changes libgcc so that it does not tickle the issue.  I hope we can
    fix the compiler at some point, but right now, this is preventing people on
    Fedora 36 systems from building compilers where the default long double is IEEE
    128-bit.
    
    I changed the three functions that deal with complex multiply and complex divide
    support (_mulkc3, _divkc3, and float128-ifunc) to always be compiled with IEEE
    128-bit long double.  This is to be type correct, and avoid mismatch declaration
    errors from the compiler.
    
    2023-01-18   Michael Meissner  <meissner@linux.ibm.com>
    
    	PR target/107299
    	* config/rs6000/_divkc3.c (top level): Require that long double is IEEE
    	128-bit.
    	(COPYSIGN): Use long double built-in.
    	(INFINITY): Likewise.
    	(FABS): Likewise.
    	(RBIG): Use TFmode constants, not KFmode.
    	(RMIN): Likewise.
    	(RMIN2): Likewise.
    	(RMINSCAL): Likewise.
    	(RMAX2): Likewise.
    	* config/rs6000/_mulkc3.c (top level): Require that long double is IEEE
    	128-bit.
    	(COPYSIGN): Use long double built-in.
    	(INFINITY): Likewise.
    	* config/rs6000/quad-float128.h (TF): Remove definition.
    	(TFtype): New macro, use _Float128 or long double.
    	(TCtype): Change to macro, use _Complex _Float128 or _Complex long
    	double.
    	(__mulkc3_sw): Only declare if long double is IEEE 128-bit.
    	(__divkc3_sw): Likewise.
    	(__mulkc3_hw): Likewise.
    	(__divkc3_hw): Likewise.
    	(__mulkc3): Likewise.
    	(__divkc3): Likewise.
    	* config/rs6000/t-rs6000 (fp128_cmuldiv): Add support to build IEEE
    	128-bit complex multiply/divide with explicit IEEE-128 long double.
    	(fp128_cmuldiv_static_obj): Likewise.
    	(fp128_cmuldiv_shared_obj): Likewise.
    	(fp128_cmuldiv_obj): Likewise.
    	(fp128_ppc_funcs): Likewise.
    	(FP128_CFLAGS_CMULDIV): Likewise.
    	* config/rs6000/t-rs6000-hw (fp128_hardfp_src): Delete, unused.
    	(fp128_cmuldiv_funcs): Add support to build IEEE 128-bit complex
    	multiply/divide with explicit IEEE-128 long double.
    	(fp128_cmuldiv_static_obj): Likewise.
    	(fp128_cmuldiv_shared_obj): Likewise.
    	(fp128_cmuldiv_obj): Likewise.
    	(fp128_hw_funcs): Likewise.
    	(fp128_ifunc_funcs): Delete, unused.
    	(fp128_ifunc_src): Likewise.
    	* libgcc2.h (TFtype): Allow md files to override this.
    	(TCtype): Likewise.
    	* soft-fp/quad.h (TFtype): Likewise.

--- libgcc/config/rs6000/quad-float128.h
+++ libgcc/config/rs6000/quad-float128.h
@@ -27,21 +27,14 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-/* quad.h defines the TFtype type by:
-   typedef float TFtype __attribute__ ((mode (TF)));
-
-   This define forces it to use KFmode (aka, ieee 128-bit floating point).
-   However, when the compiler's default is changed so that long double is IEEE
-   128-bit floating point, we need to go back to using TFmode and TCmode.  */
+/* Override quad.h's definitions for 128-bit floating point type.  */
 #ifndef __LONG_DOUBLE_IEEE128__
-#define TF KF
-
-/* We also need TCtype to represent complex ieee 128-bit float for
-   __mulkc3 and __divkc3.  */
-typedef __complex float TCtype __attribute__ ((mode (KC)));
+#define TFtype _Float128		/* long double is IBM.  */
+#define TCtype _Complex _Float128
 
 #else
-typedef __complex float TCtype __attribute__ ((mode (TC)));
+#define TFtype long double		/* long double is IEEE.  */
+#define TCtype _Complex long double
 #endif
 
 /* Force the use of the VSX instruction set.  */
@@ -100,8 +93,11 @@ extern UTItype_ppc __fixunskfti_sw (TFtype);
 #endif
 extern IBM128_TYPE __extendkftf2_sw (TFtype);
 extern TFtype __trunctfkf2_sw (IBM128_TYPE);
+
+#ifdef __LONG_DOUBLE_IEEE128__
 extern TCtype __mulkc3_sw (TFtype, TFtype, TFtype, TFtype);
 extern TCtype __divkc3_sw (TFtype, TFtype, TFtype, TFtype);
+#endif
 
 #ifdef _ARCH_PPC64
 extern TItype_ppc __fixkfti (TFtype);
@@ -146,8 +142,11 @@ extern UTItype_ppc __fixunskfti_hw (TFtype);
 #endif
 extern IBM128_TYPE __extendkftf2_hw (TFtype);
 extern TFtype __trunctfkf2_hw (IBM128_TYPE);
+
+#ifdef __LONG_DOUBLE_IEEE128__
 extern TCtype __mulkc3_hw (TFtype, TFtype, TFtype, TFtype);
 extern TCtype __divkc3_hw (TFtype, TFtype, TFtype, TFtype);
+#endif
 
 /* Ifunc function declarations, to automatically switch between software
    emulation and hardware support.  */
@@ -188,8 +187,10 @@ extern IBM128_TYPE __extendkftf2 (TFtype);
 extern TFtype __trunctfkf2 (IBM128_TYPE);
 
 /* Complex __float128 built on __float128 interfaces.  */
+#ifdef __LONG_DOUBLE_IEEE128__
 extern TCtype __mulkc3 (TFtype, TFtype, TFtype, TFtype);
 extern TCtype __divkc3 (TFtype, TFtype, TFtype, TFtype);
+#endif
 
 /* Convert IEEE 128-bit floating point to/from string.  We explicitly use
    _Float128 instead of TFmode because _strtokf and _strfromkf must be compiled
