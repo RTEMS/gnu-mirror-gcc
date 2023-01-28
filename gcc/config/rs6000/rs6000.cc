95818dbb1709301fd93fe96aa51634060263c3b8
Michael Meissner <meissner@linux.ibm.com>
2023-01-27 21:04:13 -0500

    PowerPC: Make MMA insns support DMR registers.
    This patch changes the MMA instructions to use either FPR registers
    (-mcpu=power10) or DMRs (-mcpu=future).  In this patch, the existing MMA
    instruction names are used.
    
    A macro (__PPC_DMR__) is defined if the MMA instructions use the DMRs.
    
    The patches have been tested on the following platforms.  I added the patches
    for PR target/107299 that I submitted on November 2nd before doing the builds so
    that GCC would build on systems using IEEE 128-bit long double.
        *	https://gcc.gnu.org/pipermail/gcc-patches/2022-November/604834.html
    
    There were no regressions with doing bootstrap builds and running the regression
    tests:
    
        1)	Power10 LE using --with-cpu=power10 --with-long-double-format=ieee;
        2)	Power10 LE using --with-cpu=power10 --with-long-double-format=ibm;
        3)	Power9 LE using --with-cpu=power9 --with-long-double-format=ibm; and
        4)	Power8 BE using --with-cpu=power8 (both 32-bit & 64-bit tested).
    
    Can I check this patch into the GCC 13 master branch?
    
    2023-01-27   Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/mma.md (mma_<acc>): New define_expand to handle
    	mma_<acc> for dense math and non dense math.
    	(mma_<acc> insn): Restrict to non dense math.
    	(mma_xxsetaccz): Convert to define_expand to handle non dense math and
    	dense math.
    	(mma_xxsetaccz_p10): Rename from mma_xxsetaccz and restrict usage to non
    	dense math.
    	(mma_xxsetaccz_dm): Dense math version of mma_xxsetaccz.
    	(mma_<vv>): Add support for dense math.
    	(mma_<avv>): Likewise.
    	(mma_<pv>): Likewise.
    	(mma_<apv>): Likewise.
    	(mma_<vvi4i4i8>): Likewise.
    	(mma_<avvi4i4i8>): Likewise.
    	(mma_<vvi4i4i2>): Likewise.
    	(mma_<avvi4i4i2>): Likewise.
    	(mma_<vvi4i4>): Likewise.
    	(mma_<avvi4i4>): Likewise.
    	(mma_<pvi4i2>): Likewise.
    	(mma_<apvi4i2>): Likewise.
    	(mma_<vvi4i4i4>): Likewise.
    	(mma_<avvi4i4i4>): Likewise.
    	* config/rs6000/rs6000-c.cc (rs6000_target_modify_macros): Define
    	__PPC_DMR__ if we have dense math instructions.
    	* config/rs6000/rs6000.cc (print_operand): Make %A handle only DMRs if
    	dense math and only FPRs if not dense math.
    	(rs6000_split_multireg_move): Do not generate the xxmtacc instruction to
    	prime the DMR registers or the xxmfacc instruction to de-prime
    	instructions if we have dense math register support.

--- gcc/config/rs6000/rs6000.cc
+++ gcc/config/rs6000/rs6000.cc
@@ -13909,8 +13909,13 @@ print_operand (FILE *file, rtx x, int code)
 	 overlapping with the FPR registers.  */
       if (!REG_P (x))
 	output_operand_lossage ("invalid %%A value");
-      else if (TARGET_DENSE_MATH && DMR_REGNO_P (REGNO (x)))
-	fprintf (file, "%d", REGNO (x) - FIRST_DMR_REGNO);
+      else if (TARGET_DENSE_MATH)
+	{
+	  if (DMR_REGNO_P (REGNO (x)))
+	    fprintf (file, "%d", REGNO (x) - FIRST_DMR_REGNO);
+	  else
+	    output_operand_lossage ("%%A operand is not a DMR");
+	}
       else if (!FP_REGNO_P (REGNO (x)) || (REGNO (x) % 4) != 0)
 	output_operand_lossage ("invalid %%A value");
       else
@@ -27355,7 +27360,7 @@ rs6000_split_multireg_move (rtx dst, rtx src)
 
 	  /* If we are reading an accumulator register, we have to
 	     deprime it before we can access it.  */
-	  if (TARGET_MMA
+	  if (TARGET_MMA && !TARGET_DENSE_MATH
 	      && GET_MODE (src) == XOmode && FP_REGNO_P (REGNO (src)))
 	    emit_insn (gen_mma_xxmfacc (src, src));
 
@@ -27387,9 +27392,9 @@ rs6000_split_multireg_move (rtx dst, rtx src)
 	      emit_insn (gen_rtx_SET (dst2, src2));
 	    }
 
-	  /* If we are writing an accumulator register, we have to
-	     prime it after we've written it.  */
-	  if (TARGET_MMA
+	  /* If we are writing an accumulator register that overlaps with the
+	     FPR registers, we have to prime it after we've written it.  */
+	  if (TARGET_MMA && !TARGET_DENSE_MATH
 	      && GET_MODE (dst) == XOmode && FP_REGNO_P (REGNO (dst)))
 	    emit_insn (gen_mma_xxmtacc (dst, dst));
 
@@ -27458,9 +27463,9 @@ rs6000_split_multireg_move (rtx dst, rtx src)
 	      emit_insn (gen_rtx_SET (dst_i, op));
 	    }
 
-	  /* We are writing an accumulator register, so we have to
-	     prime it after we've written it.  */
-	  if (GET_MODE (src) == XOmode)
+	  /* On systems without dense math where accumulators overlap with the
+	     vector registers, we have to prime it after we've written it.  */
+	  if (GET_MODE (src) == XOmode && !TARGET_DENSE_MATH)
 	    emit_insn (gen_mma_xxmtacc (dst, dst));
 
 	  return;
@@ -27471,9 +27476,9 @@ rs6000_split_multireg_move (rtx dst, rtx src)
 
   if (REG_P (src) && REG_P (dst) && (REGNO (src) < REGNO (dst)))
     {
-      /* If we are reading an accumulator register, we have to
-	 deprime it before we can access it.  */
-      if (TARGET_MMA
+      /* If we are reading an accumulator register and we don't have dense
+	 math, we have to deprime it before we can access it.  */
+      if (TARGET_MMA && !TARGET_DENSE_MATH
 	  && GET_MODE (src) == XOmode && FP_REGNO_P (REGNO (src)))
 	emit_insn (gen_mma_xxmfacc (src, src));
 
@@ -27501,7 +27506,7 @@ rs6000_split_multireg_move (rtx dst, rtx src)
 
       /* If we are writing an accumulator register, we have to
 	 prime it after we've written it.  */
-      if (TARGET_MMA
+      if (TARGET_MMA && !TARGET_DENSE_MATH
 	  && GET_MODE (dst) == XOmode && FP_REGNO_P (REGNO (dst)))
 	emit_insn (gen_mma_xxmtacc (dst, dst));
     }
@@ -27638,7 +27643,7 @@ rs6000_split_multireg_move (rtx dst, rtx src)
 
       /* If we are reading an accumulator register, we have to
 	 deprime it before we can access it.  */
-      if (TARGET_MMA && REG_P (src)
+      if (TARGET_MMA && !TARGET_DENSE_MATH && REG_P (src)
 	  && GET_MODE (src) == XOmode && FP_REGNO_P (REGNO (src)))
 	emit_insn (gen_mma_xxmfacc (src, src));
 
@@ -27670,7 +27675,7 @@ rs6000_split_multireg_move (rtx dst, rtx src)
 
       /* If we are writing an accumulator register, we have to
 	 prime it after we've written it.  */
-      if (TARGET_MMA && REG_P (dst)
+      if (TARGET_MMA && !TARGET_DENSE_MATH && REG_P (dst)
 	  && GET_MODE (dst) == XOmode && FP_REGNO_P (REGNO (dst)))
 	emit_insn (gen_mma_xxmtacc (dst, dst));
 
