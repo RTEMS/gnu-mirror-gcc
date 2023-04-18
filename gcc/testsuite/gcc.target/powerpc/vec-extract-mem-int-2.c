6afc0ed4c6ea4ff5671f033fc9513a49ef4810a2
Michael Meissner <meissner@linux.ibm.com>
2023-04-18 01:41:30 -0400

    Add sign/zero conversions for V4SI vec_extract
    This patch adds combiner insns to fold in sign and zero extension of vec_extract
    of V4SI with a constant element when expanding to DImode.
    
    2023-04-18   Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/vsx.md (vsx_extract_v4si_load_to_<su>di): New insn.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/vec-extract-mem-int-2.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-mem-int-2.c
@@ -0,0 +1,37 @@
+/* { dg-do compile { target lp64 } } */
+/* { dg-options "-O2 -mdejagnu-cpu=power8" } */
+/* { dg-require-effective-target p8vector_hw } */
+
+/* Test to verify that the vec_extract with constant element numbers can load
+   SImode and fold the sign/extension into the load.  */
+
+#include <altivec.h>
+
+long long
+extract_sign_v4si_0 (vector int *p)
+{
+  return vec_extract (*p, 0);		/* lwa, no extsw.  */
+}
+
+long long
+extract_sign_v4si_1 (vector int *p)
+{
+  return vec_extract (*p, 1);		/* lwa, no extsw.  */
+}
+
+unsigned long long
+extract_uns_v4si_0 (vector unsigned int *p)
+{
+  return vec_extract (*p, 0);		/* lwz, no rldicl.  */
+}
+
+unsigned long long
+extract_uns_v4si_1 (vector unsigned int *p)
+{
+  return vec_extract (*p, 1);		/* lwz, no rldicl.  */
+}
+
+/* { dg-final { scan-assembler-times {\mlwa\M}   2 } } */
+/* { dg-final { scan-assembler-times {\mlwz\M}   2 } } */
+/* { dg-final { scan-assembler-not   {\mextsw\M}   } } */
+/* { dg-final { scan-assembler-not   {\mrldicl\M}  } } */
