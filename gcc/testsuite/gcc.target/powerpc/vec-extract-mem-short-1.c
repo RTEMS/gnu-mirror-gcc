d0bc01e34e067428b4afa710ae92f00f63e272f9
Michael Meissner <meissner@linux.ibm.com>
2023-04-21 20:56:46 -0400

    Add vec_extract test cases.
    This patch adds test cases to verify that the vec_extract optimizations in
    include sign/zero extension with the load are generating code.
    
    2023-04-21   Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/vec-extract-mem-char-1.c: New test.
    	* gcc.target/powerpc/vec-extract-mem-int-1.c: New test.
    	* gcc.target/powerpc/vec-extract-mem-short-1.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-mem-short-1.c
@@ -0,0 +1,37 @@
+/* { dg-do compile { target lp64 } } */
+/* { dg-options "-O2 -mvsx" } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+
+/* Test to verify that the vec_extract with constant element numbers can load
+   HImode and fold the sign/extension into the load.  */
+
+#include <altivec.h>
+
+long long
+extract_sign_v8hi_0 (vector short *p)
+{
+  return vec_extract (*p, 0);          /* lwa, no extsw.  */
+}
+
+long long
+extract_sign_v8hi_1 (vector short *p)
+{
+  return vec_extract (*p, 1);          /* lwa, no extsw.  */
+}
+
+unsigned long long
+extract_uns_v8hi_0 (vector unsigned short *p)
+{
+  return vec_extract (*p, 0);          /* lwz, no rlwinm.  */
+}
+
+unsigned long long
+extract_uns_v8hi_1 (vector unsigned short *p)
+{
+  return vec_extract (*p, 1);          /* lwz, no rlwinm.  */
+}
+
+/* { dg-final { scan-assembler-times {\mlha\M}    2 } } */
+/* { dg-final { scan-assembler-times {\mlhz\M}    2 } } */
+/* { dg-final { scan-assembler-not   {\mextsh\M}    } } */
+/* { dg-final { scan-assembler-not   {\mrlwinm\M}   } } */
