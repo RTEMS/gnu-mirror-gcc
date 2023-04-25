fb1a7b6917d4de9e45bafc64898f2697cd42dbea
Michael Meissner <meissner@linux.ibm.com>
2023-04-24 21:47:40 -0400

    Allow consant element vec_extract to be zero or sign extended
    This patch allows vec_extract of V4SI, V8HI, and V16QI vector types with a
    constant element number to be zero extended.  It also allows vec_extract of V4SI
    and V8HI vector types with constant element number to be sign extended.
    
    2023-04-24   Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/vsx.md (vsx_extract_v4si_load_to_<su>d): New insn.
    	(vsx_extract_<VSX_EXTRACT_I2:mode>_load_to_z<GPR:mode>): New insn.
    	(vsx_extract_v8hi_load_to_z<mode>): New insn.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/vec-extract-mem-char-1.c: New file.
    	* gcc.target/powerpc/vec-extract-mem-int-1.c: New file.
    	* gcc.target/powerpc/vec-extract-mem-int-2.c: New file.
    	* gcc.target/powerpc/vec-extract-mem-short-1.c: New file.
    	* gcc.target/powerpc/vec-extract-mem-short-2.c: New file.

---
+++ gcc/testsuite/gcc.target/powerpc/vec-extract-mem-short-1.c
@@ -0,0 +1,35 @@
+/* { dg-do compile { target lp64 } } */
+/* { dg-options "-O2 -mvsx" } */
+/* { dg-require-effective-target powerpc_vsx_ok } */
+
+/* Test to verify that the vec_extract with constant element numbers can load
+   SImode and fold zero extension into the load.  */
+
+#include <altivec.h>
+
+unsigned long long
+extract_uns_v8hi_0 (vector unsigned short *p)
+{
+  return vec_extract (*p, 0);          /* lwz, no rlwinm.  */
+}
+
+unsigned long long
+extract_uns_v8hi_1 (vector unsigned short *p)
+{
+  return vec_extract (*p, 1);          /* lwz, no rlwinm.  */
+}
+
+unsigned long long
+extract_uns_v8hi_element_0_index_4 (vector unsigned short *p)
+{
+  return vec_extract (p[4], 0);		/* lbz, no rlwinm.  */
+}
+
+unsigned long long
+extract_uns_v8hi_element_3_index_4 (vector unsigned short *p)
+{
+  return vec_extract (p[4], 3);		/* lbz, no rlwinm.  */
+}
+
+/* { dg-final { scan-assembler-times {\mlhz\M}    4 } } */
+/* { dg-final { scan-assembler-not   {\mrlwinm\M}   } } */
