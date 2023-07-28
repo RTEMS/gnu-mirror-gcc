1ca0a00c51bf1dd89003aa57ce4f49655a6240cd
Michael Meissner <meissner@linux.ibm.com>
2023-07-28 13:03:19 -0400

    Update insn counts.
    2023-07-28  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/fp-overload-f64-vector.c: Update counts.

--- gcc/testsuite/gcc.target/powerpc/fp-overload-f64-vector.c
+++ gcc/testsuite/gcc.target/powerpc/fp-overload-f64-vector.c
@@ -8,15 +8,15 @@
 
 TEST (vector double, double, vect, 64)
 
-/* { dg-final { scan-assembler-times {\mvsldoi\M}    1 } } */
-/* { dg-final { scan-assembler-times {\mxvabsdp\M}   1 } } */
-/* { dg-final { scan-assembler-times {\mxvadddp\M}   2 } } */
+/* { dg-final { scan-assembler-times {\mxvabsdp\M}       1 } } */
+/* { dg-final { scan-assembler-times {\mxvadddp\M}       1 } } */
 /* { dg-final { scan-assembler-times {\mxvmadd[am]dp\M}  1 } } */
 /* { dg-final { scan-assembler-times {\mxvmsub[am]dp\M}  1 } } */
-/* { dg-final { scan-assembler-times {\mxvmuldp\M}   1 } } */
-/* { dg-final { scan-assembler-times {\mxvnabsdp\M}  1 } } */
-/* { dg-final { scan-assembler-times {\mxvnegdp\M}   1 } } */
+/* { dg-final { scan-assembler-times {\mxvmuldp\M}       1 } } */
+/* { dg-final { scan-assembler-times {\mxvnabsdp\M}      1 } } */
+/* { dg-final { scan-assembler-times {\mxvnegdp\M}       1 } } */
 /* { dg-final { scan-assembler-times {\mxvnmadd[am]dp\M} 1 } } */
 /* { dg-final { scan-assembler-times {\mxvnmsub[am]dp\M} 1 } } */
-/* { dg-final { scan-assembler-times {\mxvsubdp\M}   1 } } */
-/* { dg-final { scan-assembler-not   {\mbl\M}          } } */
+/* { dg-final { scan-assembler-times {\mxvsubdp\M}       1 } } */
+/* { dg-final { scan-assembler-times {\mxxpermdi\M}      1 } } */
+/* { dg-final { scan-assembler-not   {\mbl\M}              } } */
