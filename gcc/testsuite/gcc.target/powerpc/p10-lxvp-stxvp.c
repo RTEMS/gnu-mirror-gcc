522a66851cb83c9e12496b0420663b6a4f2f4b96
Michael Meissner <meissner@linux.ibm.com>
2023-08-30 22:06:54 -0400

    Add -mno-lxvp and -mno-stxvp.
    This patch adds the options -mno-lxvp and -mno-stxvp that control whether GCC
    will generate vector pair load/stores or split the instructions into separate
    vector loads/stores.  These switches are not documented, but are there to allow
    us to look at some places where the paired load/store instructions slow things
    down.
    
    With this patch, GCC will not allow X-form (register + register) vector
    pair loads or stores unless both lxvp and stxvp are being generated.
    
    I also added the lxvp and stxvp command line options to the options that the
    user can enable or disable with #pragma target or attribute target.
    
    The default is to generate the vector pair load/store instructions.
    
    2023-08-30  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/mma.md (movoo): Add support for -mno-lxvp and
    	-mno-stxvp.
    	* config/rs6000/rs6000.cc (rs6000_debug_reg_global): If -mdebug=reg,
    	print out whether we are generating lxvp and/or stxvp instructions.
    	(rs6000_option_override_internal): Warn if -mlxvp or -mstxvp was used
    	without -mmma being set.
    	(rs6000_setup_reg_addr_masks structure): Add support for -mno-lxvp and
    	-mno-stxvp.
    	(rs6000_opt_vars): Add lxvp and stxvp command line options.
    	* config/rs6000/rs6000.md (isa attribute): Add lxvp and stxvp isa
    	support.
    	(enabled attribute): Likewise.
    	* config/rs6000/rs6000.opt (-mlxvp): New option.
    	(-mstxvp): Likewise.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/p10-lxvp-stxvp.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/p10-lxvp-stxvp.c
@@ -0,0 +1,56 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target power10_ok } */
+/* { dg-options "-mdejagnu-cpu=power10 -O2" } */
+
+#include <stddef.h>
+
+/* Test whether we can control lxvp and stxvp.  */
+
+#pragma GCC target ("no-lxvp,no-stxvp")
+
+void
+no_vector_pair (__vector_pair *dest,
+		__vector_pair *src,
+		size_t n,
+		size_t m)
+{
+  dest[n] = src[m];		/* 2 lxv + 2 stxv.  */
+}
+
+#pragma GCC target ("lxvp,no-stxvp")
+
+void
+vector_pair_load (__vector_pair *dest,
+		  __vector_pair *src,
+		  size_t n,
+		  size_t m)
+{
+  dest[n] = src[m];		/* 1 lxvpx + 2 stxv.  */
+}
+
+#pragma GCC target ("no-lxvp,stxvp")
+
+void
+vector_pair_store (__vector_pair *dest,
+		   __vector_pair *src,
+		   size_t n,
+		   size_t m)
+{
+  dest[n] = src[m];		/* 2 lxv + 1 stxvpx.  */
+}
+
+#pragma GCC target ("lxvp,stxvp")
+
+void
+vector_pair_both (__vector_pair *dest,
+		  __vector_pair *src,
+		  size_t n,
+		  size_t m)
+{
+  dest[n] = src[m];		/* 1 lxvpx + 1 stxvpx.  */
+}
+
+/* { dg-final { scan-assembler-times {\mlxvx?\M}   4 } } */
+/* { dg-final { scan-assembler-times {\mlxvpx?\M}  2 } } */
+/* { dg-final { scan-assembler-times {\mstxvx?\M}  4 } } */
+/* { dg-final { scan-assembler-times {\mstxvpx?\M} 2 } } */
