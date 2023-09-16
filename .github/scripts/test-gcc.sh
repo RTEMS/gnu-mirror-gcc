afdf90daff33b5f8a339e2c3a1574ea50187c963
Eugene Rozenfeld <erozen@microsoft.com>
2023-09-15 17:57:49 -0700

    Use old version of validate_failures.py
    Failure manifest format and validate_failures.py were
    recently significantly changed. We'd like to keep using
    the old format of failure manifests for now.

--- .github/scripts/test-gcc.sh
+++ .github/scripts/test-gcc.sh
@@ -29,7 +29,7 @@ mkdir logs
 make -k $1 -j$(nproc) RUNTESTFLAGS="-v -v" 2>&1 >> logs/output.txt 2>&1
 
 # Compare failures found to the failures that we're expecting
-../gcc/contrib/testsuite-management/validate_failures.py >> logs/failures.txt 2>&1
+../gcc/.github/scripts/validate_failures.py >> logs/failures.txt 2>&1
 exit_code=$?
 
 # Print failures to console output so they can be viewed in the GitHub Action run
