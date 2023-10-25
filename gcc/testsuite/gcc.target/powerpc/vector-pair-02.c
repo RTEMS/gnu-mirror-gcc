9bf190e888221578c55943fffafaf61f56884119
Michael Meissner <meissner@linux.ibm.com>
2023-10-24 22:52:18 -0400

    Add support for vector pair built-in functions.
    2023-10-24  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/predicates.md (mma_assemble_input_operand): Allow other
    	16-byte vectors and not just V16QImode.
    	* config/rs6000/rs6000-builtins.def (__builtin_vpair_*): Add vector pair
    	built-in functions.
    	* config/rs6000/rs6000-protos.h (split_unary_vector_pair): Add
    	declaration.
    	(split_binary_vector_pair): Likewise.
    	(split_fma_vector_pair): Likewise.
    	* config/rs6000/rs6000.cc (split_unary_vector_pair): New helper function
    	for vector pair built-in functions.
    	(split_binary_vector_pair): Likewise.
    	(split_fma_vector_pair): Likewise.
    	* config/rs6000/rs6000.md (toplevel): Include vector-pair.md.
    	* config/rs6000/t-rs6000 (MD_INCLUDES): Add vector-pair.md.
    	* config/rs6000/vector-pair.md: New file.
    	* doc/extend.texi (PowerPC Vector Pair Built-in Functions): Document the
    	vector pair built-in functions.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/vector-pair-01.c: New test.
    	* gcc.target/powerpc/vector-pair-02.c: New test.
    	* gcc.target/powerpc/vector-pair-03.c: New test.
    	* gcc.target/powerpc/vector-pair-04.c: New test.
    	* gcc.target/powerpc/vector-pair-05.c: New test.
    	* gcc.target/powerpc/vector-pair-06.c: New test.
    	* gcc.target/powerpc/vector-pair-07.c: New test.
    	* gcc.target/powerpc/vector-pair-08.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/vector-pair-02.c
@@ -0,0 +1,176 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target power10_ok } */
+/* { dg-options "-mdejagnu-cpu=power10 -O2" } */
+
+/* Test whether the vector buitin code generates the expected instructions for
+   vector pairs with 8 float elements.  */
+
+void
+test_add (__vector_pair *dest,
+	  __vector_pair *x,
+	  __vector_pair *y)
+{
+  /* 2 lxvp, 2 xvaddsp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_add (*x, *y);
+}
+
+void
+test_sub (__vector_pair *dest,
+	  __vector_pair *x,
+	  __vector_pair *y)
+{
+  /* 2 lxvp, 2 xvsubsp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_sub (*x, *y);
+}
+
+void
+test_multiply (__vector_pair *dest,
+	       __vector_pair *x,
+	       __vector_pair *y)
+{
+  /* 2 lxvp, 2 xvmulsp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_mul (*x, *y);
+}
+
+void
+test_max (__vector_pair *dest,
+	  __vector_pair *x,
+	  __vector_pair *y)
+{
+  /* 2 lxvp, 2 xvmaxsp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_smax (*x, *y);
+}
+
+void
+test_min (__vector_pair *dest,
+	  __vector_pair *x,
+	  __vector_pair *y)
+{
+  /* 2 lxvp, 2 xvminsp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_smin (*x, *y);
+}
+
+void
+test_negate (__vector_pair *dest,
+	     __vector_pair *x)
+{
+  /* 1 lxvp, 2 xvnegsp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_neg (*x);
+}
+
+void
+test_abs (__vector_pair *dest,
+	  __vector_pair *x)
+{
+  /* 1 lxvp, 2 xvabssp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_abs (*x);
+}
+
+void
+test_negative_abs (__vector_pair *dest,
+		   __vector_pair *x)
+{
+  /* 2 lxvp, 2 xvnabssp, 1 stxvp.  */
+  __vector_pair ab = __builtin_vpair_f32_abs (*x);
+  *dest = __builtin_vpair_f32_neg (ab);
+}
+
+void
+test_fma (__vector_pair *dest,
+	  __vector_pair *x,
+	  __vector_pair *y,
+	  __vector_pair *z)
+{
+  /* 3 lxvp, 2 xvmadd{a,q}sp, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_fma (*x, *y, *z);
+}
+
+void
+test_fms (__vector_pair *dest,
+	  __vector_pair *x,
+	  __vector_pair *y,
+	  __vector_pair *z)
+{
+  /* 3 lxvp, 2 xvmsub{a,q}sp, 1 stxvp.  */
+  __vector_pair n = __builtin_vpair_f32_neg (*z);
+  *dest = __builtin_vpair_f32_fma (*x, *y, n);
+}
+
+void
+test_nfma (__vector_pair *dest,
+	   __vector_pair *x,
+	   __vector_pair *y,
+	   __vector_pair *z)
+{
+  /* 3 lxvp, 2 xvnmadd{a,q}sp, 1 stxvp.  */
+  __vector_pair w = __builtin_vpair_f32_fma (*x, *y, *z);
+  *dest = __builtin_vpair_f32_neg (w);
+}
+
+void
+test_nfms (__vector_pair *dest,
+	   __vector_pair *x,
+	   __vector_pair *y,
+	   __vector_pair *z)
+{
+  /* 3 lxvp, 2 xvnmsub{a,q}sp, 1 stxvp.  */
+  __vector_pair n = __builtin_vpair_f32_neg (*z);
+  __vector_pair w = __builtin_vpair_f32_fma (*x, *y, n);
+  *dest = __builtin_vpair_f32_neg (w);
+}
+
+void
+test_splat (__vector_pair *dest, float x)
+{
+  /* 1 xxpermdi, 1 stxvp.  */
+  *dest = __builtin_vpair_f32_splat (x);
+}
+
+void
+test_zero (__vector_pair *dest)
+{
+  /* 2 xxspltib, 1 stxvp.  */
+  *dest = __builtin_vpair_zero ();
+}
+
+vector float
+test_get_vector_0 (__vector_pair *x)
+{
+  /* 1 lxp.  */
+  return __builtin_vpair_f32_get_vector (*x, 0);
+}
+
+vector float
+test_get_vector_1 (__vector_pair *x)
+{
+  /* 1 lxp.  */
+  return __builtin_vpair_f32_get_vector (*x, 1);
+}
+
+float
+test_add_elements (__vector_pair *x)
+{
+  /* 1 lxp, 3 xvaddsp, 2 vsldoi, 1 xscvspdp.  */
+  return __builtin_vpair_f32_add_elements (*x);
+}
+
+/* { dg-final { scan-assembler-times {\mlxv\M}          2 } } */
+/* { dg-final { scan-assembler-times {\mlxvp\M}        26 } } */
+/* { dg-final { scan-assembler-times {\mstxvp\M}       14 } } */
+/* { dg-final { scan-assembler-times {\mvsldoi\M}       2 } } */
+/* { dg-final { scan-assembler-times {\mxscvdpspn\M}    1 } } */
+/* { dg-final { scan-assembler-times {\mxscvspdp\M}     1 } } */
+/* { dg-final { scan-assembler-times {\mxvabssp\M}      2 } } */
+/* { dg-final { scan-assembler-times {\mxvaddsp\M}      5 } } */
+/* { dg-final { scan-assembler-times {\mxvmadd.sp\M}    2 } } */
+/* { dg-final { scan-assembler-times {\mxvmaxsp\M}      2 } } */
+/* { dg-final { scan-assembler-times {\mxvminsp\M}      2 } } */
+/* { dg-final { scan-assembler-times {\mxvmsub.sp\M}    2 } } */
+/* { dg-final { scan-assembler-times {\mxvmulsp\M}      2 } } */
+/* { dg-final { scan-assembler-times {\mxvnabssp\M}     2 } } */
+/* { dg-final { scan-assembler-times {\mxvnegsp\M}      2 } } */
+/* { dg-final { scan-assembler-times {\mxvnmadd.sp\M}   2 } } */
+/* { dg-final { scan-assembler-times {\mxvnmsub.sp\M}   2 } } */
+/* { dg-final { scan-assembler-times {\mxvsubsp\M}      2 } } */
+/* { dg-final { scan-assembler-times {\mxxspltib\M}     2 } } */
+/* { dg-final { scan-assembler-times {\mxxspltw\M}      1 } } */
