2aa930f36c64f72a0ab72ec5ef8ffd68035a043b
Vladimir N. Makarov <vmakarov@redhat.com>
2023-10-27 08:28:24 -0400

    [RA]: Add cost calculation for reg equivalence invariants
    My recent patch improving cost calculation for pseudos with equivalence
    resulted in failure of gcc.target/arm/eliminate.c on aarch64.  This patch
    fixes this failure.
    
    gcc/ChangeLog:
    
    	* ira-costs.cc: (get_equiv_regno, calculate_equiv_gains):
    	Process reg equivalence invariants.
    
    (cherry picked from commit 8aa47713701b1f1878b81169852269a299272e87)

--- gcc/ira-costs.cc
+++ gcc/ira-costs.cc
@@ -1777,6 +1777,7 @@ get_equiv_regno (rtx x, int &regno, rtx &subreg)
     }
   if (REG_P (x)
       && (ira_reg_equiv[REGNO (x)].memory != NULL
+	  || ira_reg_equiv[REGNO (x)].invariant != NULL
 	  || ira_reg_equiv[REGNO (x)].constant != NULL))
     {
       regno = REGNO (x);
@@ -1819,6 +1820,7 @@ calculate_equiv_gains (void)
   for (regno = max_reg_num () - 1; regno >= FIRST_PSEUDO_REGISTER; regno--)
     if (ira_reg_equiv[regno].init_insns != NULL
 	&& (ira_reg_equiv[regno].memory != NULL
+	    || ira_reg_equiv[regno].invariant != NULL
 	    || (ira_reg_equiv[regno].constant != NULL
 		/* Ignore complicated constants which probably will be placed
 		   in memory:  */
@@ -1869,6 +1871,8 @@ calculate_equiv_gains (void)
 
 	  if (subst == NULL)
 	    subst = ira_reg_equiv[regno].constant;
+	  if (subst == NULL)
+	    subst = ira_reg_equiv[regno].invariant;
 	  ira_assert (subst != NULL);
 	  mode = PSEUDO_REGNO_MODE (regno);
 	  ira_init_register_move_cost_if_necessary (mode);
