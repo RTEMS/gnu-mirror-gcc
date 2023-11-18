091b04da1657c9f71d62491fa30c0846f8cb5b43
Mikael Morin <mikael@gcc.gnu.org>
2023-11-18 20:54:20 +0100

    fortran: Check for empty MINLOC/MAXLOC ARRAY along DIM only
    In the function generating inline code to implement MINLOC and MAXLOC, only
    get the size of ARRAY along DIM if DIM is present to check for emptyness.
    
    The check for ARRAY emptyness had been checking the size of the full array,
    which is correct for MINLOC and MAXLOC without DIM.  But if DIM is
    present, the reduction is along DIM only so the check for emptyness
    should consider that dimension only as well.
    
    This sounds like a correctness issue, but fortunately the cases where it
    makes a difference are cases where ARRAY is empty, so even if the MINLOC or
    MAXLOC calculated value is wrong, it's wrapped in a zero iteration loop, and
    the wrong values are not actually used.  In the end this just avoids
    unnecessary calculations.
    
    A previous version of this patch didn't support non-constant DIM with
    rank 1 ARRAY.  The new testcase checks that that case is supported.
    
    gcc/fortran/ChangeLog:
    
    	* trans-intrinsic.cc (gfc_conv_intrinsic_minmaxloc): Only get the size
    	along DIM instead of the full size if DIM is present.
    
    gcc/testsuite/ChangeLog:
    
    	* gfortran.dg/minmaxloc_22.f90: New test.

---
+++ gcc/testsuite/gfortran.dg/minmaxloc_22.f90
@@ -0,0 +1,24 @@
+! { dg-do compile }
+!
+! Check that the inline code generated for MINLOC and MAXLOC supports
+! a non-constant DIM argument if ARRAY has rank 1.
+
+program p
+  implicit none
+  integer, parameter :: n = 5
+  integer :: a(n)
+  print *, f(a, 1)
+contains
+  function f(a, d)
+    integer :: a(n)
+    integer :: d
+    integer :: f
+    f = minloc(a, dim=d) 
+  end function
+  function g(a, d)
+    integer :: a(n)
+    integer :: d
+    integer :: g
+    g = maxloc(a, dim=d) 
+  end function
+end program p
