d21e4d975e2b85f0da5957af5728ed85b4b8a808
Michael Meissner <meissner@linux.ibm.com>
2024-02-09 19:51:54 -0500

    Add debugging for -mcpu=future
    2024-02-09  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/rs6000.cc (rs6000_opt_masks): Add entry to print out
    	-mfuture in the isa flags.

--- gcc/config/rs6000/rs6000.cc
+++ gcc/config/rs6000/rs6000.cc
@@ -24505,6 +24505,7 @@ static struct rs6000_opt_mask const rs6000_opt_masks[] =
   { "float128-hardware",	OPTION_MASK_FLOAT128_HW,	false, true  },
   { "fprnd",			OPTION_MASK_FPRND,		false, true  },
   { "power10",			OPTION_MASK_POWER10,		false, true  },
+  { "future",			OPTION_MASK_FUTURE,		false, true  },
   { "hard-dfp",			OPTION_MASK_DFP,		false, true  },
   { "htm",			OPTION_MASK_HTM,		false, true  },
   { "isel",			OPTION_MASK_ISEL,		false, true  },
