e2bef5c639ab5a1b519b738cf67a03b85432d5dd
Richard Biener <rguenther@suse.de>
2024-03-07 15:36:00 +0100

    Fix last commit WRT patterns
    	* tree-vect-slp.cc (vect_analyze_slp): Look at the pattern
    	stmt for unused-only-live stmts.

--- gcc/tree-vect-slp.cc
+++ gcc/tree-vect-slp.cc
@@ -3956,10 +3956,12 @@ vect_analyze_slp (vec_info *vinfo, unsigned max_tree_size)
 		gphi *lc_phi = *gsi;
 		tree def = gimple_phi_arg_def_from_edge (lc_phi, e);
 		stmt_vec_info stmt_info;
-		if (TREE_CODE (def) == SSA_NAME
-		    && !virtual_operand_p (def)
-		    && (stmt_info = loop_vinfo->lookup_def (def))
-		    && STMT_VINFO_RELEVANT (stmt_info) == vect_used_only_live
+		if (TREE_CODE (def) != SSA_NAME
+		    || virtual_operand_p (def)
+		    || !(stmt_info = loop_vinfo->lookup_def (def)))
+		  continue;
+		stmt_info = vect_stmt_to_vectorize (stmt_info);
+		if (STMT_VINFO_RELEVANT (stmt_info) == vect_used_only_live
 		    && STMT_VINFO_LIVE_P (stmt_info)
 		    && (STMT_VINFO_DEF_TYPE (stmt_info) == vect_induction_def
 			|| (STMT_VINFO_DEF_TYPE (stmt_info) == vect_internal_def
@@ -3969,9 +3971,9 @@ vect_analyze_slp (vec_info *vinfo, unsigned max_tree_size)
 		    vec<stmt_vec_info> roots = vNULL;
 		    vec<tree> remain = vNULL;
 		    stmts.create (1);
-		    stmts.quick_push (vect_stmt_to_vectorize (stmt_info));
+		    stmts.quick_push (stmt_info);
 		    bool res = vect_build_slp_instance (vinfo,
-							slp_inst_kind_reduc_group,
+							/* ??? */ slp_inst_kind_reduc_group,
 							stmts, roots, remain,
 							max_tree_size, &limit,
 							bst_map, NULL);
