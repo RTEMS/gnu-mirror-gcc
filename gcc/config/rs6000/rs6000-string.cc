448253a197ef21591eed7b67a6d1c49baaa673e5
Michael Meissner <meissner@linux.ibm.com>
2024-03-08 22:30:22 -0500

    Revert some changes

--- gcc/config/rs6000/rs6000-string.cc
+++ gcc/config/rs6000/rs6000-string.cc
@@ -964,7 +964,6 @@ expand_compare_loop (rtx operands[])
       break;
     case PROCESSOR_POWER9:
     case PROCESSOR_POWER10:
-    case PROCESSOR_POWER11:
       if (bytes_is_const)
 	max_bytes = 191;
       else
