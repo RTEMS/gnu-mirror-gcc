5bd41ca9c05b8483af758e5010f9d000182d3e88
Michael Meissner <meissner@linux.ibm.com>
2024-03-13 02:21:06 -0400

    Add wD constraint.
    This patch adds a new constraint ('wD') that matches the accumulator registers
    that overlap with VSX registers 0..31 on power10.  Future patches will add the
    support for a separate accumulator register class that will be used when the
    support for dense math registes is added.
    
    2024-03-13   Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/constraints.md (wD): New constraint.
    	* config/rs6000/mma.md (mma_disassemble_acc): Likewise.
    	(mma_<vv>): Likewise.
    	(mma_<avv>): Likewise.
    	(mma_<pv>): Likewise.
    	(mma_<apv>): Likewise.
    	(mma_<vvi4i4i8>): Likewise.
    	(mma_<avvi4i4i8>): Likewise.
    	(mma_<vvi4i4i2>): Likewise.
    	(mma_<avvi4i4i2>): Likewise.
    	(mma_<vvi4i4>): Likewise.
    	(mma_<avvi4i4>): Likewise.
    	(mma_<pvi4i2): Likewise.
    	(mma_<apvi4i2>): Likewise.
    	(mma_<vvi4i4i4>): Likewise.
    	(mma_<avvi4i4i4): Likewise.
    	* config/rs6000/predicates.md (accumulator_operand): New predicate.
    	* config/rs6000/rs6000.cc (rs6000_debug_reg_global): Print the register
    	class for the 'wD' constraint.
    	(rs6000_init_hard_regno_mode_ok): Set the 'wD' register constraint
    	class.
    	* config/rs6000/rs6000.h (enum r6000_reg_class_enum): Add element for
    	the 'wD' constraint.
    	* doc/md.texi (PowerPC constraints): Document the 'wD' constraint.

--- gcc/config/rs6000/rs6000.cc
+++ gcc/config/rs6000/rs6000.cc
@@ -2322,6 +2322,7 @@ rs6000_debug_reg_global (void)
 	   "wr reg_class = %s\n"
 	   "wx reg_class = %s\n"
 	   "wA reg_class = %s\n"
+	   "wD reg_class = %s\n"
 	   "\n",
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_d]],
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_v]],
@@ -2329,7 +2330,8 @@ rs6000_debug_reg_global (void)
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_we]],
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wr]],
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wx]],
-	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wA]]);
+	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wA]],
+	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wD]]);
 
   nl = "\n";
   for (m = 0; m < NUM_MACHINE_MODES; ++m)
@@ -2985,6 +2987,9 @@ rs6000_init_hard_regno_mode_ok (bool global_init_p)
   /* Support for new direct moves (ISA 3.0 + 64bit).  */
   if (TARGET_DIRECT_MOVE_128)
     rs6000_constraints[RS6000_CONSTRAINT_we] = VSX_REGS;
+
+  if (TARGET_MMA)
+    rs6000_constraints[RS6000_CONSTRAINT_wD] = FLOAT_REGS;
 
   /* Set up the reload helper and direct move functions.  */
   if (TARGET_VSX || TARGET_ALTIVEC)
