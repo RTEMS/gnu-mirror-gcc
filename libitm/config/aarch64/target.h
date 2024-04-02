9f2fe2932ca35b736685753a85bd5c43459f24de
Szabolcs Nagy <szabolcs.nagy@arm.com>
2024-04-02 15:43:23 +0100

    aarch64: libitm: Add GCS support
    Transaction begin and abort use setjmp/longjmp like operations that
    need to be updated for GCS compatibility. We use similar logic to
    libc setjmp/longjmp that support switching stack and thus switching
    GCS (e.g. due to longjmp out of a makecontext stack), this is kept
    even though it is likely not required for transaction aborts.
    
    The gtm_jmpbuf is internal to libitm so we can change its layout
    without breaking ABI.
    
    libitm/ChangeLog:
    
    	* config/aarch64/sjlj.S: Add GCS support and mark GCS compatible.
    	* config/aarch64/target.h: Add gcs field to gtm_jmpbuf.

--- libitm/config/aarch64/target.h
+++ libitm/config/aarch64/target.h
@@ -30,6 +30,7 @@ typedef struct gtm_jmpbuf
   unsigned long long pc;	/* x30 */
   unsigned long long gr[10];	/* x19-x28 */
   unsigned long long vr[8];	/* d8-d15 */
+  void *gcs;			/* GCSPR_EL0 */
   void *cfa;
 } gtm_jmpbuf;
 
