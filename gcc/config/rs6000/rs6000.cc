f5177d70ff8945e98334be05bc706e133ec83cd3
Michael Meissner <meissner@linux.ibm.com>
2024-06-04 14:25:29 -0400

    Add support for the TAR register.
    2024-06-04  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/constraints.md (h constraint): Add TAR register to the
    	documentation.
    	(wt constraint): New constraint.
    	* config/rs6000/rs6000-cpus.def (ISA_3_0_MASKS_SERVER): Document that we
    	do not explicitly add -mtar for power9.
    	(OTHER_POWER10_MASKS): Add -mtar.
    	(POWERPC_MASKS): Likewise.
    	* config/rs6000/rs6000.cc (rs6000_reg_names): Add TAR register support.
    	(alt_reg_names): Likewise.
    	(rs6000_hard_regno_mode_ok_uncached): Likewise.
    	(rs6000_debug_reg_global): Print the register class that wt maps too.
    	(rs6000_init_hard_regno_mode_ok): Add TAR register support.
    	(rs6000_option_override_internal): Restrict -mtar to power9 and above.
    	(rs6000_conditional_register_usage): Add TAR register support.
    	(print_operand): Likewise.
    	(rs6000_debugger_regno): Likewise.
    	(rs6000_opt_masks): Add support for -mtar.
    	* config/rs6000/rs6000.h (FIRST_PSEUDO_REGISTER): Add TAR register
    	support.
    	(FIXED_REGISTERS): Likewise.
    	(CALL_REALLY_USED_REGISTERS): Likewise.
    	(REG_ALLOC_ORDER): Likewise.
    	(enum reg_class): Likewise.
    	(REG_CLASS_NAMES): Likewise.
    	(REG_CLASS_CONTENTS): Likewise.
    	(enum r6000_reg_class_enum): Add support for the wt constraint.
    	* config/rs6000/rs6000.md (TAR_REGNO): New constant.
    	(mov<mode>_internal): Add TAR register support.
    	(call_indirect_nonlocal_sysv<mode>): Likewise.
    	(call_value_indirect_nonlocal_sysv<mode>): Likewise.
    	(call_indirect_aix<mode>): Likewise.
    	(call_value_indirect_aix<mode>): Likewise.
    	(call_indirect_elfv2<mode>): Likewise.
    	(call_indirect_pcrel<mode>): Likewise.
    	(call_value_indirect_elfv2<mode>): Likewise.
    	(call_value_indirect_pcrel<mode>): Likewise.
    	(*sibcall_indirect_nonlocal_sysv<mode>): Likewise.
    	(sibcall_value_indirect_nonlocal_sysv<mode>): Likewise.
    	(indirect_jump<mode>): Likewise.
    	(@indirect_jump<mode>_nospec): Likewise.
    	(@tablejump<mode>_insn_normal): Likewise.
    	(@tablejump<mode>_insn_nospec): Likewise.
    	* config/rs6000/rs6000.opt (-mtar): New option.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/ppc-switch-1.c: Update test for the TAR register.
    	* gcc.target/powerpc/pr51513.c: Likewise.
    	* gcc.target/powerpc/safe-indirect-jump-2.c: Likewise.
    	* gcc.target/powerpc/safe-indirect-jump-3.c: Likewise.
    	* gcc.target/powerpc/tar-register.c: New test.

--- gcc/config/rs6000/rs6000.cc
+++ gcc/config/rs6000/rs6000.cc
@@ -1224,8 +1224,8 @@ char rs6000_reg_names[][8] =
      "lr", "ctr", "ca", "ap",
   /* cr0..cr7 */
       "0",  "1",  "2",  "3",  "4",  "5",  "6",  "7",
-  /* vrsave vscr sfp */
-      "vrsave", "vscr", "sfp",
+  /* vrsave vscr sfp, tar */
+      "vrsave", "vscr", "sfp", "tar",
 };
 
 #ifdef TARGET_REGNAMES
@@ -1250,8 +1250,8 @@ static const char alt_reg_names[][8] =
     "lr",  "ctr",   "ca",   "ap",
   /* cr0..cr7 */
   "%cr0",  "%cr1", "%cr2", "%cr3", "%cr4", "%cr5", "%cr6", "%cr7",
-  /* vrsave vscr sfp */
-  "vrsave", "vscr", "sfp",
+  /* vrsave vscr sfp, tar */
+  "vrsave", "vscr", "sfp", "tar"
 };
 #endif
 
@@ -1948,6 +1948,7 @@ rs6000_hard_regno_mode_ok_uncached (int regno, machine_mode mode)
     case VSCR_REGNO:
     case LR_REGNO:
     case CTR_REGNO:
+    case TAR_REGNO:
       {
 	unsigned reg_size = ((regno == VRSAVE_REGNO || regno == VSCR_REGNO)
 			     ? 4
@@ -2334,6 +2335,7 @@ rs6000_debug_reg_global (void)
 			  "vs");
   rs6000_debug_reg_print (LR_REGNO, LR_REGNO, "lr");
   rs6000_debug_reg_print (CTR_REGNO, CTR_REGNO, "ctr");
+  rs6000_debug_reg_print (TAR_REGNO, TAR_REGNO, "tar");
   rs6000_debug_reg_print (CR0_REGNO, CR7_REGNO, "cr");
   rs6000_debug_reg_print (CA_REGNO, CA_REGNO, "ca");
   rs6000_debug_reg_print (VRSAVE_REGNO, VRSAVE_REGNO, "vrsave");
@@ -2350,6 +2352,7 @@ rs6000_debug_reg_global (void)
 	   "wa reg_class = %s\n"
 	   "we reg_class = %s\n"
 	   "wr reg_class = %s\n"
+	   "wt reg_class = %s\n"
 	   "wx reg_class = %s\n"
 	   "wA reg_class = %s\n"
 	   "\n",
@@ -2358,6 +2361,7 @@ rs6000_debug_reg_global (void)
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wa]],
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_we]],
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wr]],
+	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wt]],
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wx]],
 	   reg_class_names[rs6000_constraints[RS6000_CONSTRAINT_wA]]);
 
@@ -2808,6 +2812,7 @@ rs6000_init_hard_regno_mode_ok (bool global_init_p)
 
   rs6000_regno_regclass[LR_REGNO] = LINK_REGS;
   rs6000_regno_regclass[CTR_REGNO] = CTR_REGS;
+  rs6000_regno_regclass[TAR_REGNO] = TAR_REGS;
   rs6000_regno_regclass[CA_REGNO] = NO_REGS;
   rs6000_regno_regclass[VRSAVE_REGNO] = VRSAVE_REGS;
   rs6000_regno_regclass[VSCR_REGNO] = VRSAVE_REGS;
@@ -2827,6 +2832,7 @@ rs6000_init_hard_regno_mode_ok (bool global_init_p)
   reg_class_to_reg_type[(int)VSCR_REGS] = SPR_REG_TYPE;
   reg_class_to_reg_type[(int)LINK_REGS] = SPR_REG_TYPE;
   reg_class_to_reg_type[(int)CTR_REGS] = SPR_REG_TYPE;
+  reg_class_to_reg_type[(int)TAR_REGS] = SPR_REG_TYPE;
   reg_class_to_reg_type[(int)LINK_OR_CTR_REGS] = SPR_REG_TYPE;
   reg_class_to_reg_type[(int)CR_REGS] = CR_REG_TYPE;
   reg_class_to_reg_type[(int)CR0_REGS] = CR_REG_TYPE;
@@ -3015,6 +3021,10 @@ rs6000_init_hard_regno_mode_ok (bool global_init_p)
   /* Support for new direct moves (ISA 3.0 + 64bit).  */
   if (TARGET_DIRECT_MOVE_128)
     rs6000_constraints[RS6000_CONSTRAINT_we] = VSX_REGS;
+
+  /* Power9 adds a TAR register that can hold the target of a jump.  */
+  if (TARGET_TAR)
+    rs6000_constraints[RS6000_CONSTRAINT_wt] = TAR_REGS;
 
   /* Set up the reload helper and direct move functions.  */
   if (TARGET_VSX || TARGET_ALTIVEC)
@@ -4222,6 +4232,15 @@ rs6000_option_override_internal (bool global_init_p)
 	error ("%qs requires %qs", "%<-mfloat128-hardware%>", "-m64");
 
       rs6000_isa_flags &= ~OPTION_MASK_FLOAT128_HW;
+    }
+
+  /* If -mtar, make sure we have at least a power9.  */
+  if (TARGET_TAR && !TARGET_P9_MISC)
+    {
+      if ((rs6000_isa_flags_explicit & OPTION_MASK_TAR) != 0)
+	error ("%qs requires %qs", "-mtar", "-mcpu=power9");
+
+      rs6000_isa_flags &= ~OPTION_MASK_TAR;
     }
 
   /* Enable -mprefixed by default on power10 systems.  */
@@ -10238,6 +10257,9 @@ rs6000_conditional_register_usage (void)
 	for (i = FIRST_ALTIVEC_REGNO + 20; i < FIRST_ALTIVEC_REGNO + 32; ++i)
 	  fixed_regs[i] = call_used_regs[i] = 1;
     }
+
+  if (TARGET_TAR)
+    fixed_regs[TAR_REGNO] = 0;
 }
 
 
@@ -14386,10 +14408,13 @@ print_operand (FILE *file, rtx x, int code)
       if (GET_CODE (x) == UNSPEC && XINT (x, 1) == UNSPEC_PLTSEQ)
 	x = XVECEXP (x, 0, 0);
       if (!REG_P (x) || (REGNO (x) != LR_REGNO
-			 && REGNO (x) != CTR_REGNO))
+			 && REGNO (x) != CTR_REGNO
+			 && REGNO (x) != TAR_REGNO))
 	output_operand_lossage ("invalid %%T value");
       else if (REGNO (x) == LR_REGNO)
 	fputs ("lr", file);
+      else if (REGNO (x) == TAR_REGNO)
+	fputs ("tar", file);
       else
 	fputs ("ctr", file);
       return;
@@ -24215,6 +24240,8 @@ rs6000_debugger_regno (unsigned int regno, unsigned int format)
 	return 108;
       if (regno == CTR_REGNO)
 	return 109;
+      if (regno == TAR_REGNO)
+	return 111;
       if (regno == CA_REGNO)
 	return 101;  /* XER */
       /* Special handling for CR for .debug_frame: rs6000_emit_prologue has
@@ -24232,7 +24259,7 @@ rs6000_debugger_regno (unsigned int regno, unsigned int format)
 
       /* These do not make much sense.  */
       if (regno == FRAME_POINTER_REGNUM)
-	return 111;
+	return FIRST_PSEUDO_REGISTER;
       if (regno == ARG_POINTER_REGNUM)
 	return 67;
       if (regno == 64)
@@ -24255,6 +24282,8 @@ rs6000_debugger_regno (unsigned int regno, unsigned int format)
     return 65;
   if (regno == CTR_REGNO)
     return 66;
+  if (regno == TAR_REGNO)
+    return 111;
   if (regno == CA_REGNO)
     return 76;  /* XER */
   if (CR_REGNO_P (regno))
@@ -24517,6 +24546,7 @@ static struct rs6000_opt_mask const rs6000_opt_masks[] =
   { "recip-precision",		OPTION_MASK_RECIP_PRECISION,	false, true  },
   { "save-toc-indirect",	OPTION_MASK_SAVE_TOC_INDIRECT,	false, true  },
   { "string",			0,				false, true  },
+  { "tar",			OPTION_MASK_TAR,		false, true  },
   { "update",			OPTION_MASK_NO_UPDATE,		true , true  },
   { "vsx",			OPTION_MASK_VSX,		false, true  },
 #ifdef OPTION_MASK_64BIT
