9148b4679335390ce0d675482f6855328708bea6
Michael Meissner <meissner@linux.ibm.com>
2024-06-14 14:50:13 -0400

    Little endian PowerPC without VSX cannot support IEEE 128-bit.
    2024-06-14  Michael Meissner  <meissner@linux.ibm.com>
    
    libgfortran/
    
    	* kinds-override.h: Do not enable IEEE 128-bit floating point support on
    	little endian PowerPC that does not have VSX support.

--- libgfortran/kinds-override.h
+++ libgfortran/kinds-override.h
@@ -30,7 +30,7 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 #endif
 
 /* Keep these conditions on one line so grep can filter it out.  */
-#if defined(__powerpc64__)  && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__  && __SIZEOF_LONG_DOUBLE__ == 16
+#if defined(__powerpc64__)  && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__  && __SIZEOF_LONG_DOUBLE__ == 16 && defined(__VSX__)
 typedef _Float128 GFC_REAL_17;
 typedef _Complex _Float128 GFC_COMPLEX_17;
 #define HAVE_GFC_REAL_17
