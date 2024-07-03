2f47c52f2a32757f9a453c74cd58ffc8dc91d6e8
Michael Meissner <meissner@linux.ibm.com>
2024-07-02 20:07:28 -0400

    Add -mcpu=power11 tests.
    This patch adds some simple tests for -mcpu=power11 support.
    
    2024-07-02  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/power11-1.c: New test.
    	* gcc.target/powerpc/power11-2.c: Likewise.
    	* gcc.target/powerpc/power11-3.c: Likewise.

---
+++ gcc/testsuite/gcc.target/powerpc/power11-1.c
@@ -0,0 +1,12 @@
+/* { dg-do assemble { target powerpc*-*-* } } */
+/* { dg-options "-mdejagnu-cpu=power11 -O2" } */
+
+/* Basic check to see if the compiler supports -mcpu=power11.  */
+
+#ifndef _ARCH_PWR11
+#error "-mcpu=power11 is not supported"
+#endif
+
+void foo (void)
+{
+}
