71db6b9e2fdff223f757c52a602e833a652244e0
Michael Meissner <meissner@linux.ibm.com>
2024-08-02 20:47:42 -0400

    Revert changes

--- gcc/config/rs6000/rs6000.h
+++ gcc/config/rs6000/rs6000.h
@@ -531,7 +531,6 @@ extern int rs6000_vector_align[];
 #define TARGET_POWER9		((rs6000_arch_flags & ARCH_MASK_POWER9)  != 0)
 #define TARGET_POWER10		((rs6000_arch_flags & ARCH_MASK_POWER10) != 0)
 #define TARGET_POWER11		((rs6000_arch_flags & ARCH_MASK_POWER11) != 0)
-#define TARGET_FUTURE		((rs6000_arch_flags & ARCH_MASK_FUTURE)  != 0)
 
 /* In the past we represented power8, power10 as an ISA bit and used internal
    switches the user was not supposed to use for -mpower8-internal and
