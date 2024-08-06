6b2ea24a882afb4639f9ea629e527aecbe82f93c
Michael Meissner <meissner@linux.ibm.com>
2024-08-06 02:35:09 -0400

    Add vector pair optimizations.
    2024-08-06  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/vector-pair.md (vpair_add_neg_<vpair_modename>3): New
    	combiner insn to convert vector plus/neg into a minus operation.
    	(vpair_fma_<vpair_modename>_merge): Optimize multiply, add/subtract, and
    	negation into fma operations if the user specifies to create fmas.
    	(vpair_fma_<vpair_modename>_merge): Likewise.
    	(vpair_fma_<vpair_modename>_merge2): Likewise.
    	(vpair_nfma_<vpair_modename>_merge): Likewise.
    	(vpair_nfms_<vpair_modename>_merge): Likewise.
    	(vpair_nfms_<vpair_modename>_merge2): Likewise.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/vector-pair-7.c: New test.
    	* gcc.target/powerpc/vector-pair-8.c: Likewise.
    	* gcc.target/powerpc/vector-pair-9.c: Likewise.
    	* gcc.target/powerpc/vector-pair-10.c: Likewise.
    	* gcc.target/powerpc/vector-pair-11.c: Likewise.
    	* gcc.target/powerpc/vector-pair-12xs.c: Likewise.

---
+++ gcc/testsuite/gcc.target/powerpc/vector-pair-8.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target power10_ok } */
+/* { dg-options "-mdejagnu-cpu=power10 -O2" } */
+
+/* Test whether the vector builtin code merges plus and neg into a minus
+   operation.  */
+
+void
+test_minus (__vector_pair *p, __vector_pair *q, __vector_pair *r)
+{
+  *p = __builtin_vpair_f32_add (*q, __builtin_vpair_f32_neg (*r));
+}
+
+/* { dg-final { scan-assembler-times {\mlxvp\M}     2 } } */
+/* { dg-final { scan-assembler-times {\mstxvp\M}    1 } } */
+/* { dg-final { scan-assembler-times {\mxvsubsp\M}  2 } } */
+/* { dg-final { scan-assembler-not   {\mxvaddsp\M}    } } */
+/* { dg-final { scan-assembler-not   {\mxvnegsp\M}    } } */
