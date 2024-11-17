b5906edb9d2e7dcc3c1c70030133ef0a3f5fb5b3
Michael Meissner <meissner@linux.ibm.com>
2024-11-17 18:13:28 -0500

    PR target/108958 -- use mtvsrdd to zero extend GPR DImode to VSX TImode
    Previously GCC would zero externd a DImode GPR value to TImode by first zero
    extending the DImode value into a GPR TImode value, and then do a MTVSRDD to
    move this value to a VSX register.
    
    This patch does the move directly, since if the middle argument to MTVSRDD is 0,
    it does the zero extend.
    
    This patch also generates LXVRDX if the DImode value is in memory.
    
    Finally, it the DImode is already in a vector register, it does a XXSPLTIB and
    XXPERMDI to get the value into the bottom 64-bits of the register.
    
    I have built GCC with the patches in this patch set applied on both little and
    big endian PowerPC systems and there were no regressions.  Can I apply this
    patch to GCC 15?
    
    2024-11-17  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* gcc/config/rs6000/rs6000.md (zero_extendditi2): New insn.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/pr108958.c: New test.

---
+++ gcc/testsuite/gcc.target/powerpc/pr108958.c
@@ -0,0 +1,58 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target int128 } */
+/* { dg-require-effective-target lp64 } */
+/* { dg-require-effective-target power10_ok } */
+/* { dg-options "-mdejagnu-cpu=power10 -O2" } */
+
+/* PR target/108958, use mtvsrdd to zero extend gpr to vsx register.  */
+
+void
+arg_to_vsx (unsigned long long x, __uint128_t *p)
+{
+  /* mtvsrdd vsx,0,gpr.  */
+  __uint128_t y = x;
+  __asm__ (" # %x0" : "+wa" (y));
+  *p = y;
+}
+
+void
+mem_to_vsx (unsigned long long *p, __uint128_t *q)
+{
+  /* lxrdx vsx,0,ptr.  */
+  __uint128_t y = *p;
+  __asm__ (" # %x0" : "+wa" (y));
+  *q = y;
+}
+
+
+void
+vsx_to_vsx (double d, __uint128_t *p)
+{
+  /* xxspltib + xxpermdir.  */
+  unsigned long long ull = d;
+  __uint128_t x = ull;
+  __asm__ (" # %x0" : "+wa" (x));
+  *p = x;
+}
+
+void
+arg_to_gpr (unsigned long long x, __uint128_t *p)
+{
+  /* mr gpr1_lo,gpr2; li gpr1_hi,0.  */
+  __uint128_t y = x;
+  __asm__ (" # %0" : "+r" (y));
+  *p = y;
+}
+
+void
+mem_to_gpr (unsigned long long *p, __uint128_t *q)
+{
+  /* ld gpr1_lo,addr; li gpr1_hi,0.  */
+  __uint128_t y = *p;
+  __asm__ (" # %0" : "+r" (y));
+  *q = y;
+}
+
+/* { dg-final { scan-assembler-times {\mmtvsrdd .*,0,.*\M} 1 } } */
+/* { dg-final { scan-assembler-times {\mlxrdx\M}           1 } } */
+/* { dg-final { scan-assembler-times {\mxxpermdi\M}        1 } } */
