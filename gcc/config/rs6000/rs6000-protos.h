65c8727f5e0f938b50998c28565d1bba0c7394ba
Michael Meissner <meissner@linux.ibm.com>
2025-01-24 11:44:01 -0500

    Revert changes

--- gcc/config/rs6000/rs6000-protos.h
+++ gcc/config/rs6000/rs6000-protos.h
@@ -115,7 +115,7 @@ extern const char *rs6000_indirect_call_template (rtx *, unsigned int);
 extern const char *rs6000_indirect_sibcall_template (rtx *, unsigned int);
 extern const char *rs6000_pltseq_template (rtx *, int);
 extern enum rtx_code rs6000_reverse_condition (machine_mode,
-					       enum rtx_code, bool);
+					       enum rtx_code);
 extern rtx rs6000_emit_eqne (machine_mode, rtx, rtx, rtx);
 extern rtx rs6000_emit_fp_cror (rtx_code, machine_mode, rtx);
 extern void rs6000_emit_sCOND (machine_mode, rtx[]);
